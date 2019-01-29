8ef71a1460f298b46cbed87ec415ca3608d7051a
Khail Saboor <khalilsaboor1@gmail.com>
2019-01-29 18:27:13 -0500

    change node and sass file

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-var http = require('http');
-var fs = require('fs')
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer(function(req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
     fs.readFile('./public/index.html', (err, fileContent) => {
         res.end(fileContent);
     })
 });
-server.listen();
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
