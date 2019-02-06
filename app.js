c41840171511dadb636fa65973f4814d3929b88f
Khail Saboor <khalilsaboor1@gmail.com>
2019-02-06 18:33:55 -0500

    edit node and css link

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const fs = require('fs');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer(function(req, res) {
+
+const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-type', 'text/html');
     fs.readFile('./public/index.html', (err, fileContent) => {
         res.end(fileContent);
     })
