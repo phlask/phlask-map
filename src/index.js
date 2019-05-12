d3f641961bf2e061247b05270ebde0675d4b0112
Gabriel Cardona <gcardonagarcia@gmail.com>
2019-05-12 14:50:35 -0400

    Adding service worker for PWA-functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { compose, withProps } from "recompose";
+import * as serviceWorker from './serviceWorker';
 import {
   withScriptjs,
   withGoogleMap,
@@ -32,3 +33,8 @@ const MyMapComponent = compose(
 ));
 
 ReactDOM.render(<MyMapComponent isMarkerShown />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
