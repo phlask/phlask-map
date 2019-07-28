0695a84fb0df49b34c8b11f5f9851e31663d6d60
Gabriel Cardona <gcardonagarcia@gmail.com>
2019-07-27 22:04:10 -0400

    fixes missed merge saves

--- src/index.js
+++ src/index.js
@@ -1,33 +1,11 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom";
-import { compose, withProps } from "recompose";
-import * as serviceWorker from './serviceWorker';
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps";
-=======
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
->>>>>>> develop
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-<<<<<<< HEAD
-ReactDOM.render(<MyMapComponent isMarkerShown />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
-=======
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
->>>>>>> develop
