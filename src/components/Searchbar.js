c5a545bbd0139b47c925ac70c25c7ef6774df818
JLap <jlap4567@gmail.com>
2019-08-27 18:19:43 -0400

    change the header to a reactbootstrap element

---
+++ src/components/Searchbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class SearchBar extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default SearchBar
