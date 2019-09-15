9fd8c2297beca806e39cfb63976c306f5d0955bd
JLap <jlap4567@gmail.com>
2019-09-15 14:33:24 -0400

    created an infowindow component for each tap

--- src/components/Head.js
+++ src/components/Head.js
@@ -43,53 +43,6 @@ export class Head extends Component {
                 </Form>
               </Navbar.Collapse>
             </Navbar>
-      
-          <Navbar bg="light" expand="lg" className="headColumns">
-            <div className = 'logo'>
-              <Navbar.Brand href="https://phlask.me/">
-                <img src={logo} alt="Logo" className="logoImage" />
-              </Navbar.Brand>
-            </div>
-            
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="#eco">Phlask EcoSystem</Nav.Link>
-                <Nav.Link href="#map">Phlask Map</Nav.Link>
-                <Nav.Link href="#share">Share Water</Nav.Link>
-                <Nav.Link href="#blog">Blog</Nav.Link>
-                <NavDropdown title="Data" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#about">
-                    About The Project
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#Business">
-                    Business Benefits
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#Finance">
-                    Financial Data
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#Citation">
-                    Citations and Acknowledgements
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#Bottle">
-                    Phlask Bottle
-                  </NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Link href="#link">Contact</Nav.Link>
-              </Nav>
-              <div id = 'searchBar'>
-                <Form inline>
-                  <FormControl
-                    type="text"
-                    placeholder="Search"
-                    className="mr-sm-2"
-                  />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
-              </div>
-              
-            </Navbar.Collapse>
-          </Navbar>
         </header>
       </div>
     );
