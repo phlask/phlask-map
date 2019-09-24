1fe5abb4738069f8f0db918c3b48ad72b7375738
Jacob <jlap4567@gmail.com>
2019-09-24 18:33:01 -0400

    change the accordian in ClosestTap to just a text changer

--- src/components/ClosestTap.js
+++ src/components/ClosestTap.js
@@ -1,44 +1,56 @@
 import React, { Component } from "react";
-import { Accordion, Card, Button } from "react-bootstrap";
+import {Card} from "react-bootstrap";
+
 
 export class ClosestTap extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        text : "Click me!"
+    };
+
+    this.change = this.change.bind(this);
+    }
+
+  change(){
+    if(this.props.lat === "" || this.props.lon === ""){
+      this.setState({
+        text: <p>
+        The closest tap feature is unavailable. We require permission
+        to access your location to provide it.
+      </p>
+      })
+    }
+    else{
+      this.setState({
+        text: <p>
+        The closest tap is: {this.props.org} <br />
+        Located at:   &nbsp;
+        <a
+          href={
+            "https://www.google.com/maps/search/?api=1&query=" +
+            this.props.lat +
+            ", " +
+            this.props.lon
+          }
+        >
+          {this.props.address}
+        </a>
+      </p>  
+      });
+    }
+  }
+
   render() {
     return (
       <div className = "closestTap">
-      <Accordion >
         <Card>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="0">
-              Click me to view the closest tap!
-            </Accordion.Toggle>
+          <Card.Header onClick={this.change}>
+              {this.state.text}
           </Card.Header>
-          <Accordion.Collapse eventKey="0">
-            <Card.Body>
-              {this.props.lat === "" || this.props.lon === "" ? (
-                <p>
-                  The closest tap feature is unavailable. We require permission
-                  to access your location to provide it.
-                </p>
-              ) : (
-                <p>
-                  The closest tap is: {this.props.org} <br />
-                  Located at:   &nbsp;
-                  <a
-                    href={
-                      "https://www.google.com/maps/search/?api=1&query=" +
-                      this.props.lat +
-                      ", " +
-                      this.props.lon
-                    }
-                  >
-                    {this.props.address}
-                  </a>
-                </p>
-              )}
-            </Card.Body>
-          </Accordion.Collapse>
+            
         </Card>
-      </Accordion>
       </div>
     );
   }
