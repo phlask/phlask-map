a6d423d772dc9531892d8c23d2f6fc1a0115f279
Jacob <jlap4567@gmail.com>
2019-11-05 19:39:42 -0500

    created function that filters displayed taps in state

--- src/components/FilterCard.js
+++ src/components/FilterCard.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import icon from "./icons8-filter.png"
-import {Card,Col,Row} from "react-bootstrap";
+import {Card,Col,Row,Form} from "react-bootstrap";
 
 export class FilterCard extends Component {
     constructor(props) {
@@ -8,8 +7,23 @@ export class FilterCard extends Component {
     
         this.state = {
             display : this.props.display,
+            ada: true,
+            filter: true,
+            tapsDisplayed: ["blue", "green", "red", "yellow"],
         };
+
+        this.legendButton = this.legendButton.bind(this);
         }
+
+    legendButton(color){
+        if(this.state.tapsDisplayed.includes(color)){
+            this.state.tapsDisplayed = this.state.tapsDisplayed.filter(item => item !== color);
+        }
+        else{
+            this.state.tapsDisplayed.push(color);
+        }
+        console.log(this.state.tapsDisplayed);
+    }
 
     render(){
         if(this.state.display === false){
@@ -24,9 +38,33 @@ export class FilterCard extends Component {
                         <Row>
                             <Col>
                                 <Card.Title>Legend</Card.Title>
+                                <div>
+                                    <img src = "https://i.imgur.com/M12e1HV.png" alt = "blue" onClick={() => {this.legendButton("blue")}}></img>
+                                </div>
+                                <div>
+                                    <img src = "https://i.imgur.com/DXMMxXR.png" alt = "green" onClick={() => {this.legendButton("green")}}></img>
+                                </div>
+                                <div>
+                                    <img src = "https://i.imgur.com/kt825XO.png" alt = "yellow" onClick={() => {this.legendButton("yellow")}}></img>
+                                </div>
+                                <div>
+                                    <img src = "https://i.imgur.com/5NOdOyY.png" alt = "red" onClick={() => {this.legendButton("red")}}></img>
+                                </div>
                             </Col>
                             <Col>
                                 <Card.Title>Filter</Card.Title>
+                            <Form>
+                                <Form.Check 
+                                    type="switch"
+                                    id="filter"
+                                    label="Filter"
+                                />
+                                <Form.Check
+                                    type="switch"
+                                    id="ada"
+                                    label="ADA"
+                                />
+                            </Form>
                             </Col>
                         </Row> 
                     </Card.Body>
