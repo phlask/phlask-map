52fa63e7197487acfd7c90ea153aa6825e8cecb1
Jacob <jlap4567@gmail.com>
2019-11-13 18:02:52 -0500

    finished frontend of filter

--- src/components/FilterCard.js
+++ src/components/FilterCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import {Card,Col,Row,Form} from "react-bootstrap";
+import "./FilterCard.css";
 
 export class FilterCard extends Component {
     constructor(props) {
@@ -10,16 +11,23 @@ export class FilterCard extends Component {
             ada: true,
             filter: true,
             tapsDisplayed: ["blue", "green", "red", "yellow"],
+            filtersOn: {
+                filter: false,
+                ada: false,
+            }
         };
 
         this.legendButton = this.legendButton.bind(this);
         }
 
+    //Toggles wether color is in the tapsDisplayed array or not
     legendButton(color){
         if(this.state.tapsDisplayed.includes(color)){
+            //removes color form tapsDisplayed array
             this.state.tapsDisplayed = this.state.tapsDisplayed.filter(item => item !== color);
         }
         else{
+            //adds color to tapsDisplayed array
             this.state.tapsDisplayed.push(color);
         }
         console.log(this.state.tapsDisplayed);
@@ -33,38 +41,50 @@ export class FilterCard extends Component {
         }
         else{
             return(
-                <Card>
+                <Card className = "filterCard">
                     <Card.Body>  
                         <Row>
-                            <Col>
-                                <Card.Title>Legend</Card.Title>
+                            <Col className = "legend">
+                                <Card.Title className = "title">Legend</Card.Title>
+                                <Row>
                                 <div>
                                     <img src = "https://i.imgur.com/M12e1HV.png" alt = "blue" onClick={() => {this.legendButton("blue")}}></img>
                                 </div>
+                                    <p className = "tapName">Public Tap</p>
+                                </Row>
+                                <Row>
                                 <div>
                                     <img src = "https://i.imgur.com/DXMMxXR.png" alt = "green" onClick={() => {this.legendButton("green")}}></img>
                                 </div>
+                                    <p className = "tapName">Private-Shared Tap</p>
+                                </Row>
+                                <Row>
                                 <div>
                                     <img src = "https://i.imgur.com/kt825XO.png" alt = "yellow" onClick={() => {this.legendButton("yellow")}}></img>
                                 </div>
+                                    <p className = "tapName">Private Tap</p>
+                                </Row>
+                                <Row>
                                 <div>
                                     <img src = "https://i.imgur.com/5NOdOyY.png" alt = "red" onClick={() => {this.legendButton("red")}}></img>
                                 </div>
+                                    <p className = "tapName">Restricted Tap</p>
+                                </Row>
                             </Col>
-                            <Col>
-                                <Card.Title>Filter</Card.Title>
-                            <Form>
-                                <Form.Check 
-                                    type="switch"
-                                    id="filter"
-                                    label="Filter"
-                                />
-                                <Form.Check
-                                    type="switch"
-                                    id="ada"
-                                    label="ADA"
-                                />
-                            </Form>
+                            <Col className = "filter">
+                                <Card.Title className = "title">Filter</Card.Title>
+                                <Form>
+                                    <Form.Check 
+                                        type="switch"
+                                        id="filtered"
+                                        label="Filtered"
+                                    />
+                                    <Form.Check
+                                        type="switch"
+                                        id="ada"
+                                        label="ADA Accessible"
+                                    />
+                                </Form>
                             </Col>
                         </Row> 
                     </Card.Body>
