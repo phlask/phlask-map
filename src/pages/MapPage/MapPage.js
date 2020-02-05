f3a2038fe3e0fbe5b9e6261d2b71593537b8f3c9
PeterArriaza <peter.arriaza@gmail.com>
2020-02-05 12:34:30 -0500

    new branch to test out redux implementation

--- src/pages/MapPage/MapPage.js
+++ src/pages/MapPage/MapPage.js
@@ -3,41 +3,56 @@ import Head from "../../components/Head";
 import ReactGoogleMaps from "./components/ReactGoogleMaps";
 
 export class MapPage extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       ada: false,
-      filter: false,
-      tapsDisplayed: ["Public", "Private", "Private-Shared", "Restricted"],
+      filtered: false,
+      tapsDisplayed: ["Public", "Private", "Private-Shared", "Restricted"]
     };
 
     this.legendButton = this.legendButton.bind(this);
+    this.toggleSwitch = this.toggleSwitch.bind(this);
   }
 
   //Toggles wether color is in the tapsDisplayed array or not
-  legendButton(color){
-    if(this.state.tapsDisplayed.includes(color)){
-        //removes color form tapsDisplayed array
-        this.state.tapsDisplayed = this.state.tapsDisplayed.filter(item => item !== color);
-        this.setState({});
-    }
+  legendButton(color) {
+    const taps = [...this.state.tapsDisplayed];
+    console.log(color, taps);
 
-    else{
-        //adds color to tapsDisplayed array
-        this.state.tapsDisplayed.push(color);
-        this.setState({})
+    if (taps.includes(color)) {
+      //removes color form tapsDisplayed array
+      const filteredTaps = taps.filter(item => item !== color);
+      this.setState({ tapsDisplayed: filteredTaps });
+    } else {
+      //adds color to tapsDisplayed array
+      this.setState({ tapsDisplayed: [...this.state.tapsDisplayed, color] });
     }
-    console.log(this.state.tapsDisplayed);
-  
+  }
+
+  toggleSwitch(type, position) {
+    switch (type) {
+      case "ada":
+        return this.setState({ ada: position });
+      case "filtered":
+        return this.setState({ filtered: position });
+    }
   }
 
   render() {
+    console.log("map page says filtered", this.state.filtered);
     return (
       <div className="MapPage">
-        <Head legendButton = {this.legendButton}/>
-        <ReactGoogleMaps tapsDisplayed = {this.state.tapsDisplayed}/>
+        <Head
+          legendButton={this.legendButton}
+          toggleSwitch={this.toggleSwitch}
+        />
+        <ReactGoogleMaps
+          tapsDisplayed={this.state.tapsDisplayed}
+          ada={this.state.ada}
+          filtered={this.state.filtered}
+        />
       </div>
     );
   }
