116906ca2daad76f13822857f4899bb35027e0c3
PeterArriaza <peter.arriaza@gmail.com>
2020-02-17 21:55:18 -0500

    fixed app.test.js to correctly import named components

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Provider from "react-redux";
+import { Provider } from "react-redux";
 import store from "./store";
+import { BrowserRouter } from "react-router-dom";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>,
     div
   );
