c947a2b5bf9e90e336aa2375344f920edcdf606a
PeterArriaza <peter.arriaza@gmail.com>
2020-04-13 21:47:09 -0400

    updated icons with figma redesign and moved filter toggle to lower popup screen with styling starting to resemble figma

--- src/components/MapPage.js
+++ src/components/MapPage.js
@@ -38,7 +38,7 @@ export class MapPage extends Component {
       case "filtered":
         return this.setState({ filtered: position });
       default:
-        return this.state
+        return this.state;
     }
   }
 
