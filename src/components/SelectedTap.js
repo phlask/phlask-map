9d5c8cf79261eca3106e4d0ac2d1251e05fcb393
brazill91 <brazill91@gmail.com>
2020-04-21 07:38:54 -0400

    styled expand arrow, icons, menu icon, and drag area

--- src/components/SelectedTap.js
+++ src/components/SelectedTap.js
@@ -13,9 +13,6 @@ import phlaskGreen from './images/phlaskGreen.png'
 import privateRestricted from './images/privateRestrictedTap.png'
 import privateShared from './images/privateSharedTap.png'
 import privateTap from './images/privateTap.png'
-// import profileImg1 from './images/profileImg1.jpg'
-// import profileImg2 from './images/profileImg2.png'
-// import profileImg3 from './images/profileImg3.jpg'
 import tapMenu from './images/tapMenu.png'
 
 const tempImages = {
@@ -34,18 +31,13 @@ const tempImages = {
 }
 export class SelectedTap extends React.Component{
 
-    expandTap(){
-        // this.setState({
-        //     infoIsExpanded: true
-        // })
-        this.props.toggleInfoExpanded(true)
-    }
-    
-    collapseTap(){
-        // this.setState({
-        //     infoIsExpanded: false
-        // })
-        this.props.toggleInfoExpanded(false)
+    toggleInfoExpanded(shouldExpand){
+        if(!shouldExpand){
+            if(!this.props.infoIsExpanded){
+                this.props.toggleInfoWindow(false)
+            }
+        }
+        this.props.toggleInfoExpanded(shouldExpand)
     }
 
     render(){
@@ -69,7 +61,7 @@ export class SelectedTap extends React.Component{
                     >
                     {/* Drag & Close Area */}
                     <div id='tap-info-top'>
-                        <div id='tap-info-drag-area'>
+                        <div id='tap-info-drag-area' onClick={()=>{this.toggleInfoExpanded(false)}}>
                             <div className='drag-bar-dash'></div>
                             <div className='drag-bar-dash'></div>
                         </div>
@@ -88,94 +80,106 @@ export class SelectedTap extends React.Component{
 
                     {/* Tap Info */}
 
-                    <div id='tap-info'>
+                    {/* Location Name */}
+                    <div id='tap-content'>
 
-                        {/* Location Name */}
-                        <div id='tap-content'>
+                        {/* Main Image */}
 
-                            {/* Main Image */}
+                        <div id='tap-info-img-box'>
+                            <img id ='tap-info-img'
+                                // src={this.props.displayImg}
+                                src={tempImages.tapImg}
+                                alt=''
+                            > 
+                            </img>
+                        </div>
 
-                            <div id='tap-info-img-box'>
-                                <img id ='tap-info-img'
-                                    // src={this.props.displayImg}
-                                    src={tempImages.tapImg}
-                                    alt=''
-                                > 
-                                </img>
+                        <div id='tap-head-info'>
+                            {/* Tap Type Icon */}
+                            <div id='tap-type-icon-container'>
+                                <div id='tap-type-icon'>
+                                    <img className='tap-info-icon-img' src={phlaskGreen} alt=''></img>
+                                </div>
                             </div>
 
-                            <div id='tap-head-info'>
-                                {/* Tap Type Icon */}
-                                <div id='tap-type-icon-container'>
-                                    <div id='tap-type-icon'>
-                                        <img className='tap-info-icon-img' src={phlaskGreen} alt=''></img>
-                                    </div>
+                            {/* Name & Address */}
+                            <div id='org-name-and-address'>
+                                <div id='tap-organization-name'>
+                                    {this.props.organization}
                                 </div>
+                                <h5 id='tap-info-address'>
+                                    {this.props.address}
+                                </h5>
+                            </div>
 
-                                {/* Name & Address */}
-                                <div id='org-name-and-address'>
-                                    <div id='tap-organization-name'>
-                                        {this.props.organization}
-                                    </div>
-                                    <h5 id='tap-info-address'>
-                                        {this.props.address}
-                                    </h5>
-                                </div>
-
-                                {/* Hours */}
-                                <div id='org-hours'>
-                                    <div id='tap-info-org-status'>Open</div>
-                                    <div id='tap-info-hours'>
-                                        <span>8am</span>-<span>10pm</span>
-                                        <div id='hours-dropdown-arrow-container'>
-                                            <img id='hours-dropdown-arrow' src={hoursArrow} alt=''></img>
-                                        </div>
+                            {/* Hours */}
+                            <div id='org-hours'>
+                                <div id='tap-info-org-status'>Open</div>
+                                <div id='tap-info-hours'>
+                                    <span>8am</span>-<span>10pm</span>
+                                    <div id='hours-dropdown-arrow-container'>
+                                        <img id='hours-dropdown-arrow' src={hoursArrow} alt=''></img>
                                     </div>
                                 </div>
                             </div>
-                            {/* Walk Time & Info Icons */}
-                            <div id='walk-time-and-icons'>
-                                <div id='tap-info-walk-time'>Estimated Walk Time: 12 mins</div>
-                                <div id='tap-info-icons-box'>
-                                    {tempImages.infoImages.map((icon,index) => (
-                                        <div className='tap-info-icon' key={index}>
-                                            <img className='tap-info-icon-img' src={icon} alt=''></img>
-                                        </div>
-                                    ))}
+                        </div>
+                        {/* Walk Time & Info Icons */}
+                        <div id='walk-time-and-icons'
+                            style={{flexDirection: this.props.infoIsExpanded
+                                ? 'column'
+                                : 'row'
+                            }}
+                        >
+                            <div id='tap-info-walk-time'>Estimated Walk Time: 12 mins</div>
+                            <div id='tap-info-icons-box'
+                                style={this.props.infoIsExpanded
+                                    ? {width: '100%'}
+                                    : {}
+                                }
+                            >
+                                {tempImages.infoImages.map((icon,index) => (
+                                    <div className='tap-info-icon' key={index}
+                                        style={this.props.infoIsExpanded
+                                            ?   {width: '4vh',
+                                                height: '4vh',
+                                                marginTop: '5px'}
+                                            : {}
+                                        }>
+                                        <img className='tap-info-icon-img' src={icon} alt=''></img>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+
+                        {/* Expanded View */}
+
+                        {this.props.infoIsExpanded
+                        ?<div id='tap-content-expanded'>
+                            
+                            {/* Description */}
+                            <div id='tap-info-description-box'>
+                                <div id='tap-info-description-expanded'>
+                                    {/* {this.props.description} */}
+                                    Lorem ipsum dolor sit amet, vix ex modus philosophia. At mei idque noluisse suavitate. Probo reprimique delicatissimi nec ut, diam mandamus te cum. Ad mea bonorum voluptua, ex quo melius fabellas efficiendi. Alii vituperatoribus vix te, per inani disputationi eu, omnium assueverit an has.
+
+    No vim verear contentiones, sed et sale nihil dictas. Ad tamquam ornatus explicari est, sea laudem volutpat maiestatis ad. Dicit paulo an eam, id wisi copiosae dissentiunt cum, has malis indoctum eu. Eam et fuisset invenire, in erant nullam liberavisse ius, ut est affert ceteros. Cotidieque neglegentur usu ex, cu nam delectus definitiones, qui eu justo dicit iriure. Qui nibh scripta ne.
+
+    Scribentur deterruisset nec ea, meis possit diceret has ut. Inermis legendos sea te. Vis in quando mollis. Ne dolore alterum vim.
+
+    Eos tollit adipisci ne, ea euismod oporteat suscipiantur eos. Cu dicant nemore aperiri pro, ex pri ubique verear platonem. Ius ut albucius probatus intellegam, id recteque adipiscing per. Ex augue commune suavitate vis. Pri in pertinax intellegebat.
+
+    Vis ei diam ridens saperet, ius ei vitae regione cotidieque. Eam ut liber sapientem, definitiones signiferumque id est, modo essent honestatis ei pro. Senserit urbanitas comprehensam ne est. Vel docendi similique ex, reque mundi percipitur vix no. Vel bonorum delenit admodum te, eos cibo oratio melius et.
                                 </div>
                             </div>
                         </div>
                         
-                                                    
-                    </div>
-                    {/* Expanded View */}
+                        /** Preview **/
 
-                    {this.props.infoIsExpanded
-                    ?<div id='tap-content-expanded'>
-                        
-                        {/* Description */}
-                        <div id='tap-info-description-box'>
-                            <div id='tap-info-description-expanded'>
-                                {/* {this.props.description} */}
-                                Lorem ipsum dolor sit amet, vix ex modus philosophia. At mei idque noluisse suavitate. Probo reprimique delicatissimi nec ut, diam mandamus te cum. Ad mea bonorum voluptua, ex quo melius fabellas efficiendi. Alii vituperatoribus vix te, per inani disputationi eu, omnium assueverit an has.
-
-No vim verear contentiones, sed et sale nihil dictas. Ad tamquam ornatus explicari est, sea laudem volutpat maiestatis ad. Dicit paulo an eam, id wisi copiosae dissentiunt cum, has malis indoctum eu. Eam et fuisset invenire, in erant nullam liberavisse ius, ut est affert ceteros. Cotidieque neglegentur usu ex, cu nam delectus definitiones, qui eu justo dicit iriure. Qui nibh scripta ne.
-
-Scribentur deterruisset nec ea, meis possit diceret has ut. Inermis legendos sea te. Vis in quando mollis. Ne dolore alterum vim.
-
-Eos tollit adipisci ne, ea euismod oporteat suscipiantur eos. Cu dicant nemore aperiri pro, ex pri ubique verear platonem. Ius ut albucius probatus intellegam, id recteque adipiscing per. Ex augue commune suavitate vis. Pri in pertinax intellegebat.
-
-Vis ei diam ridens saperet, ius ei vitae regione cotidieque. Eam ut liber sapientem, definitiones signiferumque id est, modo essent honestatis ei pro. Senserit urbanitas comprehensam ne est. Vel docendi similique ex, reque mundi percipitur vix no. Vel bonorum delenit admodum te, eos cibo oratio melius et.
-                            </div>
+                        :<div id='tap-info-arrow-box' onClick={()=>{this.toggleInfoExpanded(true)}}>
+                            <img className='tap-info-arrow' src={arrow} alt=''></img>
                         </div>
+                        }
                     </div>
-                    
-                    /** Preview **/
-
-                    :<div id='tap-info-arrow-box' onClick={this.expandTap.bind(this)}>
-                        <img className='tap-info-arrow' src={arrow} alt=''></img>
-                    </div>
-                    }
                 </div>
                 :<div></div>
                 }
