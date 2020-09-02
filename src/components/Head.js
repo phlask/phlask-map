fdf674b9bdfe452f916d8262b3e9a3d80ad4818a
Rasha Moumneh <rasha@fastmail.com>
2020-09-01 22:05:10 -0400

    Replace main menu items

--- src/components/Head.js
+++ src/components/Head.js
@@ -13,7 +13,6 @@ export class Head extends Component {
     this.state = {
       displayFilter: false
     };
-
   }
 
   render() {
@@ -22,35 +21,21 @@ export class Head extends Component {
         <header>
           <Navbar bg="light" expand="lg" className="headColumns">
             <Navbar.Brand href="https://phlask.me/">
-              <img src={logo} alt="Phlask" className="logoImage"
-                srcSet={logo + ", " + logo2x + " 2x"} />
+              <img
+                src={logo}
+                alt="Phlask"
+                className="logoImage"
+                srcSet={logo + ", " + logo2x + " 2x"}
+              />
             </Navbar.Brand>
             {/* <img src={icon} alt="filterImg" onClick={this.display} /> */}
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav" className="menu">
               <Nav className="mr-auto">
-                <Nav.Link href="#eco">Phlask EcoSystem</Nav.Link>
-                <Nav.Link href="#map">Phlask Map</Nav.Link>
-                <Nav.Link href="#share">Share Water</Nav.Link>
-                <Nav.Link href="#blog">Blog</Nav.Link>
-                <NavDropdown title="Data" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#about" className="data">
-                    About The Project
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#Business" className="data">
-                    Business Benefits
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#Finance" className="data">
-                    Financial Data
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#Citation" className="data">
-                    Citations and Acknowledgements
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#Bottle" className="data">
-                    Phlask Bottle
-                  </NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Link href="#link">Contact</Nav.Link>
+                <Nav.Link href="#mission">Mission</Nav.Link>
+                <Nav.Link href="#project">Project</Nav.Link>
+                <Nav.Link href="#share">Share Water & Food</Nav.Link>
+                <Nav.Link href="#contribute">Contribute</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
