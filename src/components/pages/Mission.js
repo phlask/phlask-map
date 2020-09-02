eecae0634d5d88fa4fd25a2182a19b25ddfb3e73
Rasha Moumneh <rasha@fastmail.com>
2020-09-01 23:05:44 -0400

    Add content to menu pages

--- src/components/pages/Mission.js
+++ src/components/pages/Mission.js
@@ -1,8 +1,33 @@
 import React from "react";
+import styles from "./Pages.module.scss";
 import Head from "../Head";
 
 const Mission = () => {
-  return <div></div>;
+  return (
+    <>
+      <Head />
+      <div className={styles.pageText}>
+        <h2>Mission</h2>
+        <h4>
+          PHLASK: a pun on the word ‘flask’ to also symbolize Philadelphia (PHL)
+          + Ask; to ‘phlask’ for water
+        </h4>
+        <p>
+          Water should remain abundant, clean and accessible to everyone.
+          PHLASK's mission is to help people find publicly available sources of
+          water, and to encourage private enterprises to provide public access
+          to their water infrastructure - simply by PHLasking. Single-use water
+          bottles are costly and wasteful. The PHLASK Ecosystem is a simple,
+          cost-effective way to reduce our dependence on single-use water
+          bottles, and an efficient solution to mitigating many of the negative
+          externalities of consuming single-use plastic water bottles. We hope
+          to normalize water-sharing and challenge the ethics of enterprises who
+          exploit public water sources and environmental sovereignty for
+          profiteering.
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Mission;
--- src/components/pages/Share.js
+++ src/components/pages/Share.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Head from "../Head";
+import styles from "./Pages.module.scss";
 
 const Share = () => {
   return (
     <>
       <Head />
+      <div className={styles.pageText}>
+        <h2>Share</h2>
+        <p>
+          Do you have access to water & food that you would like to share with
+          the public? Add yourself to the map to promote a culture of sharing
+          and PHLasking!
+        </p>
+      </div>
     </>
   );
 };
