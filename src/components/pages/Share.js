4ef22666571d4c1e8a58f5705f653101f6121f62
Rasha Moumneh <rasha@fastmail.com>
2020-09-08 19:02:50 -0400

    Clean up Contribute and Share components

--- src/components/pages/Share.js
+++ src/components/pages/Share.js
@@ -1,20 +1,16 @@
 import React from "react";
-import Head from "../Head";
 import styles from "./Pages.module.scss";
 
 const Share = () => {
   return (
-    <>
-      <Head />
-      <div className={styles.pageText}>
-        <h2>Share</h2>
-        <p>
-          Do you have access to water & food that you would like to share with
-          the public? Add yourself to the map to promote a culture of sharing
-          and PHLasking!
-        </p>
-      </div>
-    </>
+    <div className={styles.pageText}>
+      <h2>Share</h2>
+      <p>
+        Do you have access to water & food that you would like to share with the
+        public? Add yourself to the map to promote a culture of sharing and
+        PHLasking!
+      </p>
+    </div>
   );
 };
 
