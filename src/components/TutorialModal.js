6ece67c398e4c6a53a62561366ff717fff2e74e8
Rasha Moumneh <rasha@fastmail.com>
2020-09-10 15:14:23 -0400

    Create demo modal and modal button on header

---
+++ src/components/TutorialModal.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+
+const TutorialModal = ({ handleClose, show }) => {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleClose}>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default TutorialModal;
