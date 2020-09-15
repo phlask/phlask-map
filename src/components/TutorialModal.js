839af20350deb642f839d8b0031702b7b3087df8
Rasha Moumneh <rasha@fastmail.com>
2020-09-15 17:16:39 -0400

    Add icons to tutorial text

--- src/components/TutorialModal.js
+++ src/components/TutorialModal.js
@@ -5,7 +5,9 @@ import waterImg from "./images/waterButton.png";
 import foodImg from "./images/foodButton.png";
 import phlaskImg from "./images/PHLASK Button.png";
 import styles from "./TutorialModal.module.scss";
-import { isMobile } from "react-device-detect";
+import Filter from "./Filter";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSlidersH } from "@fortawesome/free-solid-svg-icons";
 
 const TutorialModal = ({ handleClose, show, handleNext, step, handlePrev }) => {
   const modalContent = {
@@ -17,32 +19,36 @@ const TutorialModal = ({ handleClose, show, handleNext, step, handlePrev }) => {
     2: {
       title: "Finding Water & Food",
       body: (
-        <>
-          <p>
-            The control panel at the bottom of the screen allows you to toggle
-            between WATER
-          </p>
-          <img src={waterImg} alt="" className={styles.modalIcon} />
-          <p>and FOOD</p>{" "}
-          <img className={styles.modalIcon} src={foodImg} alt="" />
-          <p>
-            interfaces. Select the desired interface to search the city for free
-            water and food locations. Use the center PHLASK button
-          </p>{" "}
-          <img src={phlaskImg} alt="" className={styles.modalIcon} />
-          <p>
-            to find the nearest source of water or food, depending on which
-            interface is toggled.
-          </p>
-        </>
+        <p>
+          The control panel at the bottom of the screen allows you to toggle
+          between WATER{" "}
+          <img src={waterImg} alt="" className={styles.modalIcon} /> and FOOD{" "}
+          <img className={styles.modalIcon} src={foodImg} alt="" /> interfaces.
+          Select the desired interface to search the city for free water and
+          food locations. Use the center PHLASK button{" "}
+          <img src={phlaskImg} alt="" className={styles.modalIcon} /> to find
+          the nearest source of water or food, depending on which interface is
+          toggled.
+        </p>
       )
     },
     3: {
       title: "Filter",
-      body:
-        "What’s a water app without a filter? Click the (*filter icon*) icon to filter the taps on the WATER and FOOD interfaces with more specific criteria."
+      body: (
+        <p>
+          What’s a water app without a filter? Click the{" "}
+          <FontAwesomeIcon
+            icon={faSlidersH}
+            className={styles.filterIcon}
+            size="1.5x"
+            color="#999"
+          />{" "}
+          icon to filter the taps on the WATER and FOOD interfaces with more
+          specific criteria.
+        </p>
+      )
     },
-    4: { title: "Legend", body: "case 4" }
+    4: { title: "Legend", body: "Legend" }
   };
 
   return (
