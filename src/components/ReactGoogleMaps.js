1da4b8ef8ffe07b3603f1539d3f09f216dc30225
Liz Kalter <43416298+LizKalter@users.noreply.github.com>
2020-09-16 01:05:25 -0400

    Fix for console warning about null className

--- src/components/ReactGoogleMaps.js
+++ src/components/ReactGoogleMaps.js
@@ -265,7 +265,7 @@ export class ReactGoogleMaps extends Component {
     // console.log("Rendered ReactGoogleMaps");
     
       return (
-        <div id="react-google-map" className={isMobile && "mobile-map-container"}>
+        <div id="react-google-map" className={isMobile ? "mobile-map-container" : undefined}>
           {/* <ClosestTap/> */}
           <ReactTouchEvents onTap={this.handleTap.bind(this)}>
             <div>
