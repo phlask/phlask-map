f071fba118895fdd2855884460cb1548414db76c
Liz Kalter <43416298+LizKalter@users.noreply.github.com>
2020-10-06 21:41:57 -0400

    fix css import order

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
