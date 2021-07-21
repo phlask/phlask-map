aeb9da31fd20551fc212bb4d9d25c81aecd942e4
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-07-20 21:44:47 -0500

    misc: delete unused file

--- src/components/AddResourceModal/index.js
+++ src/components/AddResourceModal/index.js
@@ -1,2 +1,1 @@
 export { default as AddResourceModal } from "./AddResourceModal";
-export { default as MetaModal } from "./MetaModal";
--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -16,7 +16,7 @@ import phlaskImg from "./images/PHLASK Button.png";
 import WaterIcon from "./icons/WaterIcon";
 import FoodIcon from "./icons/FoodIcon";
 import { isMobile } from "react-device-detect";
-import { AddResourceModal, MetaModal } from "./AddResourceModal";
+import { AddResourceModal } from "./AddResourceModal";
 
 // Actual Magic: https://stackoverflow.com/a/41337005
 // Distance calculates the distance between two lat/lon pairs
