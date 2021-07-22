971d8a979fe19de70a0a0b1e47498ee1524be333
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-07-21 21:38:08 -0500

    misc: clean up redundant components in AddResourceModal

--- src/components/AddResourceModal/AddResourceModal.js
+++ src/components/AddResourceModal/AddResourceModal.js
@@ -393,317 +393,228 @@ export class AddResourceModal extends Component {
           )}
 
           {this.state.formStep === "shareSocials" && <ShareSocials />}
+          <Form>
+            <Accordion>
+              <Accordion.Toggle as={Button} eventKey="0">
+                <h5 style={{ fontSize: "1rem", marginBottom: "10px" }}>
+                  Additional Information
+                </h5>
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="0">
+                <div className="additional-info">
+                  <Form.Group
+                    value={this.state.filtration}
+                    onChange={this.onChangeFiltration}
+                  >
+                    <Form.Label as="legend">
+                      <strong>Filtration</strong>
+                    </Form.Label>
 
-          <Modal.Header closeButton>
-            <Modal.Title>Submit a Tap!</Modal.Title>
-          </Modal.Header>
-          <Form onSubmit={this.onSubmit}>
-            <Modal.Body>
-              <Form.Group
-                controlId="Address"
-                id="time"
-                label="End Time"
-                type="time"
-                defaultValue="07:30"
-                value={this.state.address}
-                onChange={this.onChangeAddress}
-              >
-                <Form.Label>
-                  <strong>Address</strong>
-                </Form.Label>
-                <Form.Control />
-              </Form.Group>
-              <Form.Group
-                controlId="City"
-                value={this.state.city}
-                onChange={this.onChangeCity}
-              >
-                <Form.Label>
-                  <strong>City</strong>
-                </Form.Label>
-                <Form.Control />
-              </Form.Group>
-              <Form.Group
-                controlId="Description"
-                value={this.state.description}
-                onChange={this.onChangeDescription}
-              >
-                <Form.Label>
-                  <strong>Description</strong>
-                </Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows="2"
-                  placeholder="A brief description of the tap's location and appearance"
-                />
-              </Form.Group>
-              <Form.Group
-                value={this.state.accessToTap}
-                onChange={this.onChangeAccessToTap}
-              >
-                <Form.Label as="legend">
-                  <strong>Access to Tap</strong>
-                </Form.Label>
+                    <Form.Check
+                      className="radioText"
+                      type="radio"
+                      label="Yes"
+                      name="FilterRadios"
+                      id="FilterRadios1"
+                      value="yes"
+                    />
 
-                <Form.Check
-                  className="radioText"
-                  type="radio"
-                  label="Public"
-                  name="AccessRadios"
-                  id="AccessRadio1"
-                  value="public"
-                />
+                    <Form.Check
+                      className="radioText"
+                      type="radio"
+                      label="No"
+                      name="FilterRadios"
+                      id="FilterRadios2"
+                      value="no"
+                    />
+                  </Form.Group>
 
-                <Form.Check
-                  className="radioText"
-                  type="radio"
-                  label="Private"
-                  name="AccessRadios"
-                  id="AccessRadio2"
-                  value="private"
-                />
-              </Form.Group>
-              <Form.Group
-                controlId="Organization"
-                value={this.state.organization}
-                onChange={this.onChangeOrganization}
-              >
-                <Form.Label>
-                  <strong>Organization</strong>
-                </Form.Label>
-                <Form.Control placeholder="What organization/company does this tap belong to?" />
-              </Form.Group>
+                  <Form.Group
+                    value={this.state.handicapAccessable}
+                    onChange={this.onChangeHandicapAccess}
+                  >
+                    <Form.Label as="legend">
+                      <strong>Handicap Accessible</strong>
+                    </Form.Label>
 
-              <Accordion>
-                <Accordion.Toggle as={Button} eventKey="0">
-                  <h5 style={{ fontSize: "1rem", marginBottom: "10px" }}>
-                    Additional Information
-                  </h5>
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey="0">
-                  <div className="additional-info">
-                    <Form.Group
-                      value={this.state.filtration}
-                      onChange={this.onChangeFiltration}
-                    >
-                      <Form.Label as="legend">
-                        <strong>Filtration</strong>
-                      </Form.Label>
+                    <Form.Check
+                      className="radioText"
+                      type="radio"
+                      label="Yes"
+                      name="HandicapRadios"
+                      id="HandicapRadios1"
+                      value="yes"
+                    />
 
-                      <Form.Check
-                        className="radioText"
-                        type="radio"
-                        label="Yes"
-                        name="FilterRadios"
-                        id="FilterRadios1"
-                        value="yes"
-                      />
+                    <Form.Check
+                      className="radioText"
+                      type="radio"
+                      label="No"
+                      name="HandicapRadios"
+                      id="HandicapRadios2"
+                      value="no"
+                    />
+                  </Form.Group>
 
-                      <Form.Check
-                        className="radioText"
-                        type="radio"
-                        label="No"
-                        name="FilterRadios"
-                        id="FilterRadios2"
-                        value="no"
-                      />
-                    </Form.Group>
+                  <Form.Group
+                    value={this.state.tapServiceType}
+                    onChange={this.onChangeTapServiceType}
+                  >
+                    <Form.Label as="legend">
+                      <strong>Tap Service Type</strong>
+                    </Form.Label>
 
-                    <Form.Group
-                      value={this.state.handicapAccessable}
-                      onChange={this.onChangeHandicapAccess}
-                    >
-                      <Form.Label as="legend">
-                        <strong>Handicap Accessible</strong>
-                      </Form.Label>
+                    <Form.Check
+                      className="radioText"
+                      type="radio"
+                      label="Self-serve"
+                      name="ServiceRadios"
+                      id="ServiceRadios1"
+                      value="self serve"
+                    />
 
-                      <Form.Check
-                        className="radioText"
-                        type="radio"
-                        label="Yes"
-                        name="HandicapRadios"
-                        id="HandicapRadios1"
-                        value="yes"
-                      />
+                    <Form.Check
+                      className="radioText"
+                      type="radio"
+                      label="Ask proprietor"
+                      name="ServiceRadios"
+                      id="ServiceRadios2"
+                      value="ask proprietor"
+                    />
+                  </Form.Group>
 
-                      <Form.Check
-                        className="radioText"
-                        type="radio"
-                        label="No"
-                        name="HandicapRadios"
-                        id="HandicapRadios2"
-                        value="no"
-                      />
-                    </Form.Group>
+                  <Form.Group
+                    value={this.state.tapType}
+                    onChange={this.onChangeTapType}
+                  >
+                    <Form.Label as="legend">
+                      <strong>Tap Type</strong>
+                    </Form.Label>
 
-                    <Form.Group
-                      value={this.state.tapServiceType}
-                      onChange={this.onChangeTapServiceType}
-                    >
-                      <Form.Label as="legend">
-                        <strong>Tap Service Type</strong>
-                      </Form.Label>
+                    <Form.Check
+                      className="radioText"
+                      type="radio"
+                      label="Drinking Foutain"
+                      name="TypeRadios"
+                      id="TypeRadios1"
+                      value="drinking fountain"
+                    />
 
-                      <Form.Check
-                        className="radioText"
-                        type="radio"
-                        label="Self-serve"
-                        name="ServiceRadios"
-                        id="ServiceRadios1"
-                        value="self serve"
-                      />
+                    <Form.Check
+                      className="radioText"
+                      type="radio"
+                      label="Bottle filler and fountain"
+                      name="TypeRadios"
+                      id="TypeRadios2"
+                      value="bottle filter and fountain"
+                    />
 
-                      <Form.Check
-                        className="radioText"
-                        type="radio"
-                        label="Ask proprietor"
-                        name="ServiceRadios"
-                        id="ServiceRadios2"
-                        value="ask proprietor"
-                      />
-                    </Form.Group>
+                    <Form.Check
+                      className="radioText"
+                      type="radio"
+                      label="Sink"
+                      name="TypeRadios"
+                      id="TypeRadios3"
+                      value="sink"
+                    />
 
-                    <Form.Group
-                      value={this.state.tapType}
-                      onChange={this.onChangeTapType}
-                    >
-                      <Form.Label as="legend">
-                        <strong>Tap Type</strong>
-                      </Form.Label>
+                    <Form.Check
+                      className="radioText"
+                      type="radio"
+                      label="Soda fountain"
+                      name="TypeRadios"
+                      id="TypeRadios4"
+                      value="soda fountain"
+                    />
 
-                      <Form.Check
-                        className="radioText"
-                        type="radio"
-                        label="Drinking Foutain"
-                        name="TypeRadios"
-                        id="TypeRadios1"
-                        value="drinking fountain"
-                      />
+                    <Form.Check
+                      className="radioText"
+                      type="radio"
+                      label="Dedicated water dispenser"
+                      name="TypeRadios"
+                      id="TypeRadios5"
+                      value="dedicated water dispenser"
+                    />
 
-                      <Form.Check
-                        className="radioText"
-                        type="radio"
-                        label="Bottle filler and fountain"
-                        name="TypeRadios"
-                        id="TypeRadios2"
-                        value="bottle filter and fountain"
-                      />
+                    <Form.Check
+                      className="radioText"
+                      type="radio"
+                      label="Water cooler"
+                      name="TypeRadios"
+                      id="TypeRadios6"
+                      value="water cooler"
+                    />
 
-                      <Form.Check
-                        className="radioText"
-                        type="radio"
-                        label="Sink"
-                        name="TypeRadios"
-                        id="TypeRadios3"
-                        value="sink"
-                      />
+                    <Form.Check
+                      className="radioText"
+                      type="radio"
+                      label="Other"
+                      name="TypeRadios"
+                      id="TypeRadios7"
+                      value="other"
+                    />
+                  </Form.Group>
 
-                      <Form.Check
-                        className="radioText"
-                        type="radio"
-                        label="Soda fountain"
-                        name="TypeRadios"
-                        id="TypeRadios4"
-                        value="soda fountain"
-                      />
+                  <Form.Group
+                    value={this.state.waterVessleNeeded}
+                    onChange={this.onChangeWaterVessleNeeded}
+                  >
+                    <Form.Label as="legend">
+                      <strong>Water Vessel Needed</strong>
+                    </Form.Label>
 
-                      <Form.Check
-                        className="radioText"
-                        type="radio"
-                        label="Dedicated water dispenser"
-                        name="TypeRadios"
-                        id="TypeRadios5"
-                        value="dedicated water dispenser"
-                      />
+                    <Form.Check
+                      className="radioText"
+                      type="radio"
+                      label="Yes"
+                      name="VesselRadios"
+                      id="VesselRadios1"
+                      value="yes"
+                    />
 
-                      <Form.Check
-                        className="radioText"
-                        type="radio"
-                        label="Water cooler"
-                        name="TypeRadios"
-                        id="TypeRadios6"
-                        value="water cooler"
-                      />
+                    <Form.Check
+                      className="radioText"
+                      type="radio"
+                      label="No"
+                      name="VesselRadios"
+                      id="VesselRadios2"
+                      value="no"
+                    />
+                  </Form.Group>
 
-                      <Form.Check
-                        className="radioText"
-                        type="radio"
-                        label="Other"
-                        name="TypeRadios"
-                        id="TypeRadios7"
-                        value="other"
-                      />
-                    </Form.Group>
+                  <Form.Group
+                    controlId="Phlask Statement"
+                    value={this.state.phlaskStatement}
+                    onChange={this.onChangePhlaskStatement}
+                  >
+                    <Form.Label>
+                      <strong>PHLASK Statement</strong>
+                    </Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      rows="2"
+                      placeholder="Please use this section to make any statement about your organization or enterprise!"
+                    />
+                  </Form.Group>
 
-                    <Form.Group
-                      value={this.state.waterVessleNeeded}
-                      onChange={this.onChangeWaterVessleNeeded}
-                    >
-                      <Form.Label as="legend">
-                        <strong>Water Vessel Needed</strong>
-                      </Form.Label>
+                  <Form.Group
+                    controlId="Norms and Rules"
+                    value={this.state.normsAndRules}
+                    onChange={this.onChangeNormsAndRules}
+                  >
+                    <Form.Label>
+                      <strong>Norms and Rules</strong>
+                    </Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      rows="3"
+                      placeholder="PHLASKing is intended to be an unobtrusive part of doing business. If there are special norms associated with accessing water, please use this space to describe them."
+                    />
+                  </Form.Group>
+                </div>
+              </Accordion.Collapse>
+            </Accordion>
 
-                      <Form.Check
-                        className="radioText"
-                        type="radio"
-                        label="Yes"
-                        name="VesselRadios"
-                        id="VesselRadios1"
-                        value="yes"
-                      />
-
-                      <Form.Check
-                        className="radioText"
-                        type="radio"
-                        label="No"
-                        name="VesselRadios"
-                        id="VesselRadios2"
-                        value="no"
-                      />
-                    </Form.Group>
-
-                    <Form.Group
-                      controlId="Phlask Statement"
-                      value={this.state.phlaskStatement}
-                      onChange={this.onChangePhlaskStatement}
-                    >
-                      <Form.Label>
-                        <strong>PHLASK Statement</strong>
-                      </Form.Label>
-                      <Form.Control
-                        as="textarea"
-                        rows="2"
-                        placeholder="Please use this section to make any statement about your organization or enterprise!"
-                      />
-                    </Form.Group>
-
-                    <Form.Group
-                      controlId="Norms and Rules"
-                      value={this.state.normsAndRules}
-                      onChange={this.onChangeNormsAndRules}
-                    >
-                      <Form.Label>
-                        <strong>Norms and Rules</strong>
-                      </Form.Label>
-                      <Form.Control
-                        as="textarea"
-                        rows="3"
-                        placeholder="PHLASKing is intended to be an unobtrusive part of doing business. If there are special norms associated with accessing water, please use this space to describe them."
-                      />
-                    </Form.Group>
-                  </div>
-                </Accordion.Collapse>
-              </Accordion>
-
-              <ImageUploader
-                withIcon={true}
-                buttonText="Choose images"
-                onChange={this.onDrop}
-                imgExtension={[".jpg", ".png", ".gif"]}
-                maxFileSize={5242880}
-                withPreview={true}
-              />
-            </Modal.Body>
             <Modal.Footer>
               <Button variant="secondary" onClick={this.handleClose}>
                 Close
