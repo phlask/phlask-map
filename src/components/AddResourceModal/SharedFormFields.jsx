4538a470fb9342bd33030192d68ae3181c9e49de
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-07-24 11:25:02 -0500

    misc: remove modal wrapper - unneeded extra div
    In the parent components that the fragment gets placed into
    is already wrapped within a Modal.Body -- so we shouldn't wrap
    it again in another Modal.Body internally

--- src/components/AddResourceModal/SharedFormFields.jsx
+++ src/components/AddResourceModal/SharedFormFields.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Modal, Form, Button } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 import ImageUploader from "react-images-upload";
 
 function SharedFormFields({
@@ -17,63 +17,58 @@ function SharedFormFields({
 }) {
   return (
     <>
-      <Modal.Body>
-        <ImageUploader
-          withIcon={true}
-          buttonText="Choose images"
-          onChange={onDrop}
-          imgExtension={[".jpg", ".png", ".gif"]}
-          maxFileSize={5242880}
-          withPreview={true}
-        />
+      <ImageUploader
+        withIcon={true}
+        buttonText="Choose images"
+        onChange={onDrop}
+        imgExtension={[".jpg", ".png", ".gif"]}
+        maxFileSize={5242880}
+        withPreview={true}
+      />
 
-        <Form.Group controlId="name" value={name} onChange={onNameChange}>
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            text="text"
-            placeholder="Organization, store, facility, etc."
-          />
-        </Form.Group>
-        <Form.Group
-          controlId="address"
-          value={address}
-          onChange={onAddressChange}
-        >
-          <Form.Label>Address</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter the address of this tap"
-          />
-        </Form.Group>
-        <Form.Group
-          controlId="website"
-          value={website}
-          onChange={onWebsiteChange}
-        >
-          <Form.Label>Website</Form.Label>
-          <Form.Control type="text" placeholder="https://" />
-        </Form.Group>
-        <Form.Group
-          controlId="description"
-          value={description}
-          onChange={onDescriptionChange}
-        >
-          <Form.Label>Description</Form.Label>
-          <Form.Control
-            type="textarea"
-            rows="2"
-            placeholder="Please describe the water site location"
-          />
-        </Form.Group>
-        <Form.Group
-          controlId="Organization"
-          value={organization}
-          onChange={onChangeOrganization}
-        >
-          <Form.Label>Organization</Form.Label>
-          <Form.Control placeholder="Who does this tap belong to?" />
-        </Form.Group>
-      </Modal.Body>
+      <Form.Group controlId="name" value={name} onChange={onNameChange}>
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          text="text"
+          placeholder="Organization, store, facility, etc."
+        />
+      </Form.Group>
+      <Form.Group
+        controlId="address"
+        value={address}
+        onChange={onAddressChange}
+      >
+        <Form.Label>Address</Form.Label>
+        <Form.Control type="text" placeholder="Enter the address of this tap" />
+      </Form.Group>
+      <Form.Group
+        controlId="website"
+        value={website}
+        onChange={onWebsiteChange}
+      >
+        <Form.Label>Website</Form.Label>
+        <Form.Control type="text" placeholder="https://" />
+      </Form.Group>
+      <Form.Group
+        controlId="description"
+        value={description}
+        onChange={onDescriptionChange}
+      >
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          type="textarea"
+          rows="2"
+          placeholder="Please describe the water site location"
+        />
+      </Form.Group>
+      <Form.Group
+        controlId="Organization"
+        value={organization}
+        onChange={onChangeOrganization}
+      >
+        <Form.Label>Organization</Form.Label>
+        <Form.Control placeholder="Who does this tap belong to?" />
+      </Form.Group>
     </>
   );
 }
