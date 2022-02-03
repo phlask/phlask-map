93dbc79393220188b70ca4916cc3c9a8b509a4a4
jmcfadd <justinmcfadd4@gmail.com>
2022-02-02 19:25:42 -0500

    Add menu button

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -1,11 +1,18 @@
-import { Box } from "@mui/material";
+import { AppBar, Box, IconButton, SvgIcon, Toolbar } from "@mui/material";
 import React from "react";
 import "./Head.css";
+import {ReactComponent as MenuIcon} from "../icons/HamburgerMenu.svg";
 
 export default function Head() {
   return (
     <Box>
-      
+      <AppBar>
+        <Toolbar>
+          <IconButton>
+            <SvgIcon component={MenuIcon} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     </Box>
   );
 }
