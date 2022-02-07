4914fce4d306a615c7679533a0d0e8e30a9379e5
jmcfadd <justinmcfadd4@gmail.com>
2022-02-07 17:19:38 -0500

    Add right justification for search and filters

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -1,28 +1,28 @@
 import { AppBar, Box, IconButton, SvgIcon, Toolbar } from "@mui/material";
 import React from "react";
 import "./Head.css";
-import {ReactComponent as MenuIcon} from "../icons/HamburgerMenu.svg";
-import {ReactComponent as PhlaskIcon} from "../icons/PHLASK_v2.svg";
-import {ReactComponent as SearchIcon} from "../icons/SearchIcon.svg";
-import {ReactComponent as SlidersIcon} from "../icons/SlidersIcon.svg";
+import { ReactComponent as MenuIcon } from "../icons/HamburgerMenu.svg";
+import { ReactComponent as PhlaskIcon } from "../icons/PHLASK_v2.svg";
+import { ReactComponent as SearchIcon } from "../icons/SearchIcon.svg";
+import { ReactComponent as SlidersIcon } from "../icons/SlidersIcon.svg";
 
 export default function Head() {
   return (
     <Box>
       <AppBar>
-        <Toolbar>
+        <Toolbar className="appbar">
           <IconButton>
             <SvgIcon component={MenuIcon} />
           </IconButton>
-          <IconButton>
-            <SvgIcon component={PhlaskIcon} />
-          </IconButton>
-          <IconButton>
-            <SvgIcon component={SearchIcon} />
-          </IconButton>
-          <IconButton>
-            <SvgIcon component={SlidersIcon} />
-          </IconButton>
+          <SvgIcon className="phlask-logo" component={PhlaskIcon} />
+          <Box className="right-group">
+            <IconButton>
+              <SvgIcon component={SearchIcon} />
+            </IconButton>
+            <IconButton>
+              <SvgIcon component={SlidersIcon} />
+            </IconButton>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
