02012644924491b43753a159b55122e58d482681
jmcfadd <justinmcfadd4@gmail.com>
2022-02-07 16:45:14 -0500

    Add Phlask v2 icon

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -2,6 +2,7 @@ import { AppBar, Box, IconButton, SvgIcon, Toolbar } from "@mui/material";
 import React from "react";
 import "./Head.css";
 import {ReactComponent as MenuIcon} from "../icons/HamburgerMenu.svg";
+import {ReactComponent as PhlaskIcon} from "../icons/PHLASK_v2.svg";
 
 export default function Head() {
   return (
@@ -10,6 +11,9 @@ export default function Head() {
         <Toolbar>
           <IconButton>
             <SvgIcon component={MenuIcon} />
+          </IconButton>
+          <IconButton>
+            <SvgIcon component={PhlaskIcon} />
           </IconButton>
         </Toolbar>
       </AppBar>
