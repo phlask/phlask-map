5e9f570451b8a1db5d331839e356f0d48078b199
jmcfadd <justinmcfadd4@gmail.com>
2022-02-28 18:23:30 -0500

    Move from CSS to MUI styles

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -1,29 +1,49 @@
-import { AppBar, Box, IconButton, SvgIcon, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  IconButton, styled, SvgIcon,
+  Toolbar
+} from "@mui/material";
 import React from "react";
-import "./Head.css";
+import { toggleSearchBar } from "../../actions/actions";
 import { ReactComponent as MenuIcon } from "../icons/HamburgerMenu.svg";
 import { ReactComponent as PhlaskIcon } from "../icons/PHLASK_v2.svg";
 import { ReactComponent as SearchIcon } from "../icons/SearchIcon.svg";
 import { ReactComponent as SlidersIcon } from "../icons/SlidersIcon.svg";
-import { toggleSearchBar } from "../../actions/actions"
 
+const HeadIcon = styled(SvgIcon)(({ theme }) => ({
+  overflow: "visible",
+  color: "transparent"
+}));
 
 export default function Head() {
-
   return (
     <Box>
       <AppBar>
-        <Toolbar className="appbar">
+        <Toolbar
+          sx={{
+            backgroundColor: "#fff",
+            color: "#fff",
+            boxShadow:
+              "0 1px 0 rgba(0, 0, 0, 0.12), 0 1px 0 rgba(0, 0, 0, 0.24)",
+            display: "flex"
+          }}
+        >
           <IconButton>
-            <SvgIcon component={MenuIcon} />
+            <HeadIcon component={MenuIcon} />
           </IconButton>
-          <SvgIcon className="phlask-logo" component={PhlaskIcon} />
-          <Box className="right-group">
+          <HeadIcon component={PhlaskIcon} />
+          <Box
+            sx={{
+              marginLeft: "auto",
+              marginTop: "-1%"
+            }}
+          >
             <IconButton onClick={toggleSearchBar}>
-              <SvgIcon  className="right-icon" component={SearchIcon} />
+              <HeadIcon component={SearchIcon} />
             </IconButton>
             <IconButton>
-              <SvgIcon className="right-icon" component={SlidersIcon} />
+              <HeadIcon component={SlidersIcon} />
             </IconButton>
           </Box>
         </Toolbar>
