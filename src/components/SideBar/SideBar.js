febc4ddbf9955392023d953762b92ef3521bb941
jmcfadd <justinmcfadd4@gmail.com>
2022-02-28 19:20:17 -0500

    Add Sidebar toggle

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,9 +1,26 @@
-import React, { Component } from "react";
+import { Drawer, SvgIcon } from '@mui/material';
+import React from 'react'
+import { ReactComponent as PhlaskIcon } from "../icons/PHLASK_v2.svg";
 
-export class SideBar extends Component {
-  render() {
-    return <div></div>;
-  }
+
+
+export default function SideBar({ open, setOpen }) {
+  return (
+    <Drawer
+      anchor="left"
+      variant="temporary"
+      open={open}
+      onClose={() => setOpen(false)}
+      sx={{
+        width: "100%",
+        "& .MuiDrawer-paper": {
+          width: "70%",
+          height: "100%"
+        }
+      }}
+    >
+      <SvgIcon sx={{overflow: "visible"}} component={PhlaskIcon} />
+    </Drawer>
+  );
 }
 
-export default SideBar;
