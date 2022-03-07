9792bd8d5c3336a22a8ad225f86a9df817e5b209
jmcfadd <justinmcfadd4@gmail.com>
2022-03-07 17:19:36 -0500

    Add menu to sidebar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,8 +1,19 @@
-import { Drawer, SvgIcon } from '@mui/material';
-import React from 'react'
+import {
+  Drawer,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  SvgIcon
+} from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
 import { ReactComponent as PhlaskIcon } from "../icons/PHLASK_v2.svg";
-
-
+import { ReactComponent as CloseIcon } from "../icons/CloseIcon.svg";
+import { ReactComponent as UsersIcon } from "../icons/UsersIcon.svg";
+import { ReactComponent as PlusCircleIcon } from "../icons/PlusCircle.svg";
+import { ReactComponent as PhlaskNoTextIcon } from "../icons/PhlaskNoText.svg";
 
 export default function SideBar({ open, setOpen }) {
   return (
@@ -19,8 +30,47 @@ export default function SideBar({ open, setOpen }) {
         }
       }}
     >
-      <SvgIcon sx={{overflow: "visible"}} component={PhlaskIcon} />
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          height: "50px"
+        }}
+      >
+        <IconButton>
+          <SvgIcon
+            sx={{
+              overflow: "visible",
+              backgroundColor: "transparent",
+              boxShadow: "none",
+              marginRight: "15px"
+            }}
+            component={CloseIcon}
+          />
+        </IconButton>
+        <CloseIcon />
+        <PhlaskIcon />
+      </Box>
+      <List>
+        <ListItemButton>
+          <ListItemIcon>
+            <PhlaskNoTextIcon />
+          </ListItemIcon>
+          <ListItemText>About</ListItemText>
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <PlusCircleIcon />
+          </ListItemIcon>
+          <ListItemText>Add Resources</ListItemText>
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <UsersIcon />
+          </ListItemIcon>
+          <ListItemText>Join Team</ListItemText>
+        </ListItemButton>
+      </List>
     </Drawer>
   );
 }
-
