85d695075b9dfc79cd4a7621539bd7c667e08934
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-04-05 18:28:22 -0400

    Added transition to FilterV2

--- src/components/Filter/FilterV2.js
+++ src/components/Filter/FilterV2.js
@@ -1,4 +1,12 @@
-import { Box, IconButton, Modal, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Link,
+  IconButton,
+  Modal,
+  Stack,
+  Slide,
+  Typography
+} from "@mui/material";
 import React, { useState } from "react";
 import { ReactComponent as FoodIcon } from "../icons/FoodIconV2.svg";
 import { ReactComponent as ForagingIcon } from "../icons/ForagingIconV2.svg";
@@ -17,40 +25,54 @@ const FilterV2 = () => {
         <PhlaskFilterIcon />
       </IconButton>
       <Modal open={open} onClose={handleClose}>
-        <Stack spacing={2} m={2} maxWidth={"sm"}>
-          <Stack direction="row">
-            <WaterIcon />
-            <Box display={"flex"} alignItems={"end"}>
-              <Typography mx={1.5} bgcolor={"white"} p={0.5} borderRadius={1}>
-                Water
-              </Typography>
-            </Box>
+        <Slide
+          direction="up"
+          in={open}
+          mountOnEnter
+          unmountOnExit
+          timeout={250}
+        >
+          <Stack spacing={2} m={2} maxWidth={175}>
+            <Stack direction="row">
+              <WaterIcon />
+              <Box display={"flex"} alignItems={"end"}>
+                <Typography
+                  color={"black"}
+                  mx={1.5}
+                  bgcolor={"white"}
+                  p={0.5}
+                  borderRadius={1}
+                >
+                  Water
+                </Typography>
+              </Box>
+            </Stack>
+            <Stack direction="row">
+              <FoodIcon />
+              <Box display={"flex"} alignItems={"end"}>
+                <Typography mx={1.5} bgcolor={"white"} p={0.5} borderRadius={1}>
+                  Food
+                </Typography>
+              </Box>
+            </Stack>
+            <Stack direction="row">
+              <ForagingIcon />
+              <Box display={"flex"} alignItems={"end"}>
+                <Typography mx={1.5} bgcolor={"white"} p={0.5} borderRadius={1}>
+                  Foraging
+                </Typography>
+              </Box>
+            </Stack>
+            <Stack direction="row">
+              <ToiletIcon />
+              <Box display={"flex"} alignItems={"end"}>
+                <Typography mx={1.5} bgcolor={"white"} p={0.5} borderRadius={1}>
+                  Toilets
+                </Typography>
+              </Box>
+            </Stack>
           </Stack>
-          <Stack direction="row">
-            <FoodIcon />
-            <Box display={"flex"} alignItems={"end"}>
-              <Typography mx={1.5} bgcolor={"white"} p={0.5} borderRadius={1}>
-                Food
-              </Typography>
-            </Box>
-          </Stack>
-          <Stack direction="row">
-            <ForagingIcon />
-            <Box display={"flex"} alignItems={"end"}>
-              <Typography mx={1.5} bgcolor={"white"} p={0.5} borderRadius={1}>
-                Foraging
-              </Typography>
-            </Box>
-          </Stack>
-          <Stack direction="row">
-            <ToiletIcon />
-            <Box display={"flex"} alignItems={"end"}>
-              <Typography mx={1.5} bgcolor={"white"} p={0.5} borderRadius={1}>
-                Toilets
-              </Typography>
-            </Box>
-          </Stack>
-        </Stack>
+        </Slide>
       </Modal>
     </>
   );
