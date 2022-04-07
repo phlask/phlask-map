f9a7b7d07a947bb71584251ca07a8bd0542a7736
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-04-07 00:06:05 -0400

    List version of the Filter

---
+++ src/components/Filter/ListFilter.js
@@ -0,0 +1,131 @@
+import {
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Modal,
+  Slide,
+  Typography
+} from "@mui/material";
+import React, { useState } from "react";
+import { ReactComponent as FoodIcon } from "../icons/FoodIconV2.svg";
+import { ReactComponent as ForagingIcon } from "../icons/ForagingIconV2.svg";
+import { ReactComponent as ResourcesPin } from "../icons/ResourcesPin.svg";
+import { ReactComponent as ToiletIcon } from "../icons/ToiletIconV2.svg";
+import { ReactComponent as WaterIcon } from "../icons/WaterIconV2.svg";
+
+const ListFilter = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <IconButton
+        onClick={handleOpen}
+        size="large"
+        sx={{
+          "&.MuiButtonBase-root:hover": {
+            bgcolor: "transparent"
+          }
+        }}
+      >
+        <Box>
+          <ResourcesPin />
+          <Typography size="small" color={"black"}>
+            Resources
+          </Typography>
+        </Box>
+      </IconButton>
+      <Modal open={open} onClose={handleClose}>
+        <Slide
+          direction="up"
+          in={open}
+          mountOnEnter
+          unmountOnExit
+          timeout={250}
+        >
+          <List sx={{ maxWidth: 250 }}>
+            <ListItem sx={{ alignItems: "end" }}>
+              <ListItemIcon>
+                <WaterIcon />
+              </ListItemIcon>
+              <ListItemText>
+                <Typography
+                  variant="button1"
+                  textAlign={"center"}
+                  color={"black"}
+                  mx={1.5}
+                  bgcolor={"white"}
+                  p={0.5}
+                  borderRadius={1}
+                  px={1}
+                >
+                  Water
+                </Typography>
+              </ListItemText>
+            </ListItem>
+            <ListItem sx={{ alignItems: "end" }}>
+              <ListItemIcon>
+                <FoodIcon />
+              </ListItemIcon>
+              <ListItemText>
+                <Typography
+                  variant="button1"
+                  textAlign={"center"}
+                  mx={1.5}
+                  bgcolor={"white"}
+                  p={0.5}
+                  borderRadius={1}
+                  px={1}
+                >
+                  Food
+                </Typography>
+              </ListItemText>
+            </ListItem>
+            <ListItem sx={{ alignItems: "end" }}>
+              <ListItemIcon>
+                <ForagingIcon />
+              </ListItemIcon>
+              <ListItemText>
+                <Typography
+                  variant="button1"
+                  textAlign={"center"}
+                  mx={1.5}
+                  bgcolor={"white"}
+                  p={0.5}
+                  borderRadius={1}
+                  px={1}
+                >
+                  Foraging
+                </Typography>
+              </ListItemText>
+            </ListItem>
+            <ListItem sx={{ alignItems: "end" }}>
+              <ListItemIcon>
+                <ToiletIcon />
+              </ListItemIcon>
+              <ListItemText>
+                <Typography
+                  variant="button1"
+                  textAlign={"center"}
+                  mx={1.5}
+                  bgcolor={"white"}
+                  p={0.5}
+                  borderRadius={1}
+                  px={1}
+                >
+                  Bathroom
+                </Typography>
+              </ListItemText>
+            </ListItem>
+          </List>
+        </Slide>
+      </Modal>
+    </>
+  );
+};
+
+export default ListFilter;
