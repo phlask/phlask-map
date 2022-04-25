3624e18a237811b17458d7c72a9b0eb14d16b6f1
jmcfadd <justinmcfadd4@gmail.com>
2022-04-24 21:36:22 -0400

    Move distance functions to separate file

---
+++ src/components/Toolbar/distanceFunctions.js
@@ -0,0 +1,63 @@
+
+// Actual Magic: https://stackoverflow.com/a/41337005
+// Distance calculates the distance between two lat/lon pairs
+export function distance(lat1, lon1, lat2, lon2) {
+  var p = 0.017453292519943295;
+  var a =
+    0.5 -
+    Math.cos((lat2 - lat1) * p) / 2 +
+    (Math.cos(lat1 * p) *
+      Math.cos(lat2 * p) *
+      (1 - Math.cos((lon2 - lon1) * p))) /
+      2;
+  return 12742 * Math.asin(Math.sqrt(a));
+}
+
+// Takes an array of objects with lat and lon properties as well as a single object with lat and lon
+// properties and finds the closest point (by shortest distance).
+function getClosest(data, userLocation) {
+  // console.log(data.map(p => distance(v['lat'],v['lon'],p['lat'],p['lon'])))
+  // console.log(Math.min(...data.map(p => distance(v['lat'],v['lon'],p['lat'],p['lon']))))
+  var distances = data.map((org, index) => {
+    return {
+      lat: org["lat"],
+      lon: org["lon"],
+      organization: org["organization"],
+      address: org["address"],
+      distance: distance(
+        userLocation["lat"],
+        userLocation["lon"],
+        org["lat"],
+        org["lon"]
+      ),
+      id: index
+    };
+  });
+  var minDistance = Math.min(...distances.map(d => d.distance));
+
+  var closestTap = {
+    organization: "",
+    address: "",
+    lat: "",
+    lon: "",
+    id: ""
+  };
+
+  for (var i = 0; i < distances.length; i++) {
+    if (distances[i].distance === minDistance) {
+      closestTap.lat = distances[i].lat;
+      closestTap.lon = distances[i].lon;
+      closestTap.organization = distances[i].organization;
+      closestTap.address = distances[i].address;
+      closestTap.id = distances[i].id;
+    }
+  }
+
+  return closestTap;
+}
+
+function getCoordinates() {
+  return new Promise(function(resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
