2defa38f8c3f7d0428ad80f0fa7b107d477ddedd
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-05-31 23:40:53 -0400

    Initial actionSanitizer to clean up performance of Redux dev tools

--- src/store.js
+++ src/store.js
@@ -2,10 +2,18 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import filterMarkers from "./reducers/filterMarkers";
 
+// const actionSanitizer = (action) =>
+// action.type === "GET_TAPS_SUCCESS" && action.data
+// ? { ...action, data: "<<LONG_BLOB>>" }
+// : action;
+
 const composeEnhancers =
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
     trace: true,
     traceLimit: 25,
+    // actionSanitizer,
+    // stateSanitizer: (state) =>
+    // state.data ? { ...state, data: "<<LONG_BLOB>>" } : state,
   }) || compose;
 
 const store = createStore(
