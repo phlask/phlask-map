014f769955bf147b92687df1fbe9d91de06b49c0
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-06-14 18:13:18 -0400

    Using reduxjs/toolkit's configureStore() to simplify store configuration

--- src/store.js
+++ src/store.js
@@ -1,24 +1,10 @@
-import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import filterMarkers from "./reducers/filterMarkers";
+import { configureStore } from "@reduxjs/toolkit";
 
-const actionSanitizer = (action) =>
-  action.type === "GET_TAPS_SUCCESS" && action.data
-    ? { ...action, data: "<<LONG_BLOB>>" }
-    : action;
-
-const composeEnhancers =
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-    trace: true,
-    traceLimit: 25,
-    actionSanitizer,
-    stateSanitizer: (state) =>
-      state.data ? { ...state, data: "<<LONG_BLOB>>" } : state,
-  }) || compose;
-
-const store = createStore(
-  filterMarkers,
-  composeEnhancers(applyMiddleware(thunk))
-);
+const store = configureStore({
+  reducer: filterMarkers,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware([thunk]),
+});
 
 export default store;
