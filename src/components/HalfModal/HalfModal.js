a0b8ea4680e3a42945c54093439b187621f970a0
ying <97061798+yingmo55@users.noreply.github.com>
2022-07-06 18:22:42 -0400

    Temp commit

--- src/components/HalfModal/HalfModal.js
+++ src/components/HalfModal/HalfModal.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import { SwipeableDrawer } from '@mui/material';
+import React, {useState} from 'react';
+import { SwipeableDrawer, Button, Collapse } from '@mui/material';
 
 export default function HalfModal(props) {
+
+    const [ toggleCollapse, setToggleCollapse ] = useState(false)
+
+    const toggleBtn = () => {
+        setToggleCollapse((prev)=> !prev)
+    }
 
     return(
         <SwipeableDrawer anchor='bottom'
@@ -11,6 +17,10 @@ export default function HalfModal(props) {
                 PaperProps={{ square: false }}
                 >
             {props.children}
+            <button onClick={toggleBtn}>test</button>
+            <Collapse in={toggleCollapse} timeout="auto" unmountOnExit>
+                <p>more things to the bottom?</p>
+            </Collapse>
         </SwipeableDrawer>
     )
 }
