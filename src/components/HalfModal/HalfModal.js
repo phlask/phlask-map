c0743c659328c37203dca114b5b209c1a7131518
ying <97061798+yingmo55@users.noreply.github.com>
2022-07-12 17:35:39 -0400

    Add information for Full Modal
    - Adjust how props are passed to HalfModalInfo.js
    - Add useState and useEffect for rendering tags on each tap
    - Add collapsible menu for additional information
    - Add styles for additional informations
    Note:
    - "test" button is temprorary

--- src/components/HalfModal/HalfModal.js
+++ src/components/HalfModal/HalfModal.js
@@ -1,14 +1,7 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { SwipeableDrawer, Button, Collapse } from '@mui/material';
 
 export default function HalfModal(props) {
-
-    const [ toggleCollapse, setToggleCollapse ] = useState(false)
-
-    const toggleBtn = () => {
-        setToggleCollapse((prev)=> !prev)
-    }
-
     return(
         <SwipeableDrawer anchor='bottom'
                 open={props.open}
@@ -17,10 +10,6 @@ export default function HalfModal(props) {
                 PaperProps={{ square: false }}
                 >
             {props.children}
-            <button onClick={toggleBtn}>test</button>
-            <Collapse in={toggleCollapse} timeout="auto" unmountOnExit>
-                <p>more things to the bottom?</p>
-            </Collapse>
         </SwipeableDrawer>
     )
 }
--- src/components/HalfModal/HalfModalInfo.js
+++ src/components/HalfModal/HalfModalInfo.js
@@ -1,11 +1,21 @@
 import styles from './HalfModalInfo.module.scss';
-import React from 'react';
-import { Button } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import React, { useState, useEffect } from 'react';
+import { Button, Collapse } from '@mui/material';
 import directionButton from '../images/ArrowElbowUpRight.svg'
 
 function HalfModalInfo(props) {
+    
+  const [ tags, setTags ] = useState([])
+  const [ toggleCollapse, setToggleCollapse ] = useState(false)
 
-  const {nameOfPlace, addressOfPlace, imageOfPlace, estWalkTime, iconSrc } = props;
+  const { imageOfPlace, estWalkTime, selectedPlace } = props;
+  // nameOfPlace={this.state.organization}
+  // addressOfPlace={this.state.address}
+  // estWalkTime={this.state.walkingDuration}
+  // iconSrc={this.props.selectedPlace.infoIcon}
+  
+  const {organization, address, infoIcon } = selectedPlace;
 
   const btnstyle =  {
       padding: '6px 20px 6px 25px',
@@ -20,15 +30,63 @@ function HalfModalInfo(props) {
     return <img src={directionButton} alt='' />
   }
 
+  console.log(selectedPlace)
+
+  const { filtration, handicap, service, sparkling, tap_type, vessel } = props.selectedPlace;
+
+  const  { description, norms_rules } = props.selectedPlace;
+
+  const BootstrapButton = styled(Button)({
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 14,
+    color: '#2D3748',
+    padding: '2px 5px',
+    margin: '2px 5px',
+    border: '1px solid #2D3748',
+    lineHeight: 1.5,
+  });
+  
+
+
+  useEffect(()=> {
+      const showTags = () => {
+          const shownTags = []
+          if (filtration === "yes") {
+              shownTags.push("Filtered")
+          }
+          if (handicap === "yes") {
+              shownTags.push("ADA Accessible")
+          }
+          if (service) {
+            shownTags.push(service)
+        }
+          if (vessel != "yes") {
+              shownTags.push("Vessel Needed")
+          }
+
+        if (tap_type) {
+          shownTags.push(tap_type)
+      }
+          return shownTags
+      }
+      setTags(showTags())
+  }, [])
+
+
+  const toggleBtn = () => {
+    setToggleCollapse((prev)=> !prev)
+}
+
   return (
-    <div className={styles.halfInfo}>
+<div className={styles.halfInfo}>
         <span className={styles.swipeIcon}></span>
         <img src={imageOfPlace} className={styles.locationImage}  alt='' />
         <div className={styles.mainHalfInfo}>
-          { iconSrc && <img src={iconSrc} alt='' /> }
+          { infoIcon && <img src={infoIcon} alt='' /> }
             <div className={styles.mainHalfInfoText}>
-                <h2 className={styles.nameOfPlace}>{nameOfPlace}</h2>
-                <p className={styles.addressOfPlace}>{addressOfPlace}</p>
+                <h2 className={styles.nameOfPlace}>{organization}</h2>
+                <p className={styles.addressOfPlace}>{address}</p>
                 {/* <p><span className={styles.locationOpen}>Open</span> - Closes 10PM</p> */}
                 {props.children}
                 <Button variant="contained" disableElevation sx={btnstyle} startIcon={<RightArrow />}>
@@ -36,7 +94,32 @@ function HalfModalInfo(props) {
                 <p className={styles.estWalkTime}>Est. walking time: <span className={styles.walkTime}>{estWalkTime}min</span></p>
             </div>
         </div>
-    </div>
+
+    <button onClick={toggleBtn}>test</button>
+    <Collapse in={toggleCollapse} timeout="auto" unmountOnExit>
+        <hr/>
+        <div className={styles.halfInfoExpand}>
+          <div className={styles.tagGroup}>
+            {tags.map((tag,  index)=>
+                <BootstrapButton size="small" variant="outlined" key={index}>{tag}</BootstrapButton>
+            )}
+          </div>
+        <hr/>
+          <div className={styles.details}>
+            <h3>Description</h3>
+            <p>{description ? description : "N/A" }</p>
+          </div>
+          <div className={styles.details}>
+            <h3>PHLASK Statement</h3>
+            <p>N/A</p>
+          </div>
+          <div className={styles.details}>
+            <h3>Norms &#38; Rules </h3>
+            <p>{norms_rules ? norms_rules : "N/A"}</p>
+          </div>
+        </div>
+    </Collapse>
+</div>
   );
 }
 
