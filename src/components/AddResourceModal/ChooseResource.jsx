f3fdfa135f0ea85b4b12c5dd6616c4e6f808e935
ncatin <34193416+ncatin@users.noreply.github.com>
2023-03-14 20:31:30 -0400

    Updates to Choose Resourse menu (#297)
    * Updates to Choose Resourse menu
    
    * Adds temporary button to render AddResourceModalV2 on mobile and tunes presentation for all buttons and water button content
    
    * updated button colors on hover
    
    * feat(choose-resource): update icons
    
    * Integrates AddResourceModalV2 with Toolbar button
    
    * Updates yarn lock
    
    * Fixes mix up with foraging and bathroom icons
    
    * changes to theme.js
    
    * Adjusts top padding on buttons to align to prototype
    
    * Updates desktop toolbar to include Add Resource button
    
    ---------
    
    Co-authored-by: Gabriel Cardona <2278918+gcardonag@users.noreply.github.com>
    Co-authored-by: Ron Braha <ron.braha@icloud.com>

--- src/components/AddResourceModal/ChooseResource.jsx
+++ src/components/AddResourceModal/ChooseResource.jsx
@@ -1,43 +1,57 @@
-import React from "react";
-import styles from "./AddResourceModal.module.scss";
-import { Modal, Button } from "react-bootstrap";
+import React from 'react';
+import styles from './AddResourceModal.module.scss';
+import Button from '@mui/material/Button';
+import { ReactComponent as WaterIconCR } from '../icons/WaterIconChooseResource.svg';
+import { ReactComponent as FoodIconCR } from '../icons/FoodIconChooseResource.svg';
+import { ReactComponent as ForagingIconCR } from '../icons/ForagingIconChooseResource.svg';
+import { ReactComponent as ToiletIconCR } from '../icons/ToiletIconChooseResource.svg';
 
 function ChooseResource({ setFormStep }) {
   return (
-    <div className={styles.modalContent}>
-      <Modal.Header className={styles.modalHeader} closeButton></Modal.Header>
-      <h1 className={styles.greyHeader}>Add a Resource</h1>
-      <Button
-        className={styles.modalButton}
-        variant="primary"
-        onClick={() => setFormStep("addWaterTap")}
-      >
-        Water Tap
-      </Button>
-      <Button
-        className={styles.modalButton}
-        variant="primary"
-        onClick={() => setFormStep("addFood")}
-      >
-        Food
-      </Button>
-      <Button
-        className={styles.modalButton}
-        variant="primary"
-        onClick={() => setFormStep("addBathroom")}
-      >
-        Bathroom
-      </Button>
-      {/* this copy is different than the copy from the figma page,
-        this might be a bit more clear? can make a point to ask 
-        about this next week */}
-      <Button
-        className={styles.modalButton}
-        variant="primary"
-        onClick={() => setFormStep("addForaging")}
-      >
-        Foraging
-      </Button>
+    <div className={styles.dialog}>
+      <h2 className={styles.greyHeader}>Add a Resource</h2>
+      <h3 className={styles.subHeader}>
+        Choose the type of resource you like
+        <br />
+        to add and submit the form.
+      </h3>
+      <div className={styles.buttonWrapper}>
+        <Button
+          className={styles.modalButton}
+          variant="water"
+          onClick={() => setFormStep('addWaterTap')}
+        >
+          <WaterIconCR />
+          <span>Water</span>
+        </Button>
+        <Button
+          className={styles.modalButton}
+          variant="food"
+          onClick={() => setFormStep('addFood')}
+        >
+          <FoodIconCR />
+          Food
+        </Button>
+        <Button
+          className={styles.modalButton}
+          variant="bathrooms"
+          onClick={() => setFormStep('addBathroom')}
+        >
+          <ToiletIconCR />
+          Bathroom
+        </Button>
+        {/* this copy is different than the copy from the figma page,
+          this might be a bit more clear? can make a point to ask 
+          about this next week */}
+        <Button
+          className={styles.modalButton}
+          variant="foraging"
+          onClick={() => setFormStep('addForaging')}
+        >
+          <ForagingIconCR />
+          Foraging
+        </Button>
+      </div>
     </div>
   );
 }
