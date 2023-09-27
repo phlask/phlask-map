1dcbcb00adabfcaaede90d921bf25ba55bc47dc8
Joseph <joeoani@gmail.com>
2023-09-26 20:58:06 -0400

    settings

--- src/components/SelectedTap/SelectedTap.js
+++ src/components/SelectedTap/SelectedTap.js
@@ -272,9 +272,8 @@ class SelectedTap extends React.Component {
                 height: '700px'
               }}
             >
-
               {/* <DialogTitle>Dialog Title</DialogTitle> */}
-            
+
               <IconButton
                 aria-label="close"
                 onClick={() => {
@@ -290,96 +289,95 @@ class SelectedTap extends React.Component {
               >
                 <CloseIcon />
               </IconButton>
-            
 
-            {/* Location Name */}
-            <div
-              ref={this.refContentArea}
-              className={
-                this.props.infoIsExpanded
-                  ? styles.tapContentExpanded
-                  : styles.tapContent
-              }
-            >
-              {/* Main Image */}
+              {/* Location Name */}
+              <div
+                ref={this.refContentArea}
+                className={
+                  this.props.infoIsExpanded
+                    ? styles.tapContentExpanded
+                    : styles.tapContent
+                }
+              >
+                {/* Main Image */}
 
-              <div id="tap-info-img-box-desktop">
-                <img
-                  id="tap-info-img"
-                  src={tempImages.tapImg}
-                  srcSet={
-                    tempImages.tapImg + ', ' + tempImages.tapImg2x + ' 2x'
-                  }
-                ></img>
-              </div>
-                  {/* Main Image */}
+                <div id="tap-info-img-box-desktop">
+                  <img-alt
+                    id="tap-info-img"
+                    src={tempImages.tapImg}
+                    srcSet={
+                      tempImages.tapImg + ', ' + tempImages.tapImg2x + ' 2x'
+                    }
+                  ></img-alt>
+                </div>
+                {/* Main Image */}
 
-                  <div id="tap-head-info">
-                    {/* Tap Type Icon */}
-                    <div id="tap-type-icon-container">
-                      <div id="tap-type-icon">
-                        {this.props.phlaskType === PHLASK_TYPE_WATER ? (
-                          <img
-                            className="tap-info-icon-img"
-                            src={this.props.selectedPlace?.infoIcon}
-                            alt=""
-                          ></img>
-                        ) : (
-                          this.props.selectedPlace?.infoIcon
+                <div id="tap-head-info">
+                  {/* Tap Type Icon */}
+                  <div id="tap-type-icon-container">
+                    <div id="tap-type-icon">
+                      {this.props.phlaskType === PHLASK_TYPE_WATER ? (
+                        <img
+                          className="tap-info-icon-img"
+                          src={this.props.selectedPlace?.infoIcon}
+                          alt=""
+                        ></img>
+                      ) : (
+                        this.props.selectedPlace?.infoIcon
+                      )}
+                    </div>
+                  </div>
+
+                  {/* Name & Address */}
+                  <div id="org-name-and-address-desktop">
+                    <div id="tap-organization-name">
+                      {this.state.organization}
+                    </div>
+                    {this.state.address && (
+                      <h5 id="tap-info-address">{this.state.address}</h5>
+                    )}
+                  </div>
+
+                  <SelectedTapHours
+                    infoIsExpanded={this.props.infoIsExpanded}
+                    selectedPlace={this.props.selectedPlace}
+                  />
+                </div>
+                {/* Walk Time & Info Icons  */}
+                <div className={styles.walkTime}>
+                  Estimated Walk Time: {this.state.walkingDuration} mins (
+                  {this.state.walkingDistance} mi)
+                </div>
+
+                <SelectedTapIcons place={this.props.selectedPlace} />
+
+                {/* Description */}
+                <div>
+                  <div>
+                    <div className={styles.description}>
+                      <div id="tap-info-description">
+                        {this.state.tapDescription && (
+                          <div className={styles.section}>
+                            <h3>Description</h3>
+                            <div>{this.state.tapDescription}</div>
+                          </div>
+                        )}
+                        {this.state.tapStatement && (
+                          <div className={styles.section}>
+                            <h3>Statement</h3>
+                            <div>{this.state.tapStatement}</div>
+                          </div>
+                        )}
+                        {this.state.tapNormsAndRules && (
+                          <div className={styles.section}>
+                            <h3>Norms &amp; Rules</h3>
+                            <div>{this.state.tapNormsAndRules}</div>
+                          </div>
                         )}
                       </div>
                     </div>
-
-                    {/* Name & Address */}
-                    <div id="org-name-and-address-desktop">
-                      <div id="tap-organization-name">
-                        {this.state.organization}
-                      </div>
-                      {this.state.address && (
-                        <h5 id="tap-info-address">{this.state.address}</h5>
-                      )}
-                    </div>
-
-                    <SelectedTapHours
-                      infoIsExpanded={this.props.infoIsExpanded}
-                      selectedPlace={this.props.selectedPlace}
-                    />
                   </div>
-                  {/* Walk Time & Info Icons  */}
-                  <div className={styles.walkTime}>
-                    Estimated Walk Time: {this.state.walkingDuration} mins (
-                    {this.state.walkingDistance} mi)
-                  </div>
-
-                  <SelectedTapIcons place={this.props.selectedPlace} />
-
-                  {/* Description */}
-                  <div>
-                    <div>
-                      <div className={styles.description}>
-                        <div id="tap-info-description">
-                          {this.state.tapDescription && (
-                            <div className={styles.section}>
-                              <h3>Description</h3>
-                              <div>{this.state.tapDescription}</div>
-                            </div>
-                          )}
-                          {this.state.tapStatement && (
-                            <div className={styles.section}>
-                              <h3>Statement</h3>
-                              <div>{this.state.tapStatement}</div>
-                            </div>
-                          )}
-                          {this.state.tapNormsAndRules && (
-                            <div className={styles.section}>
-                              <h3>Norms &amp; Rules</h3>
-                              <div>{this.state.tapNormsAndRules}</div>
-                            </div>
-                          )}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                </div>
               </div>
             </Paper>
           </div>
