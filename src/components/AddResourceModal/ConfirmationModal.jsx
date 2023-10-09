4dc35ad57c0c088625ea737f0f1deed69c6fd7b7
alexa <alexamacias192@gmail.com>
2023-10-09 17:26:06 -0400

    Creates a ConfirmationModal file, includes basic elements and imports

---
+++ src/components/AddResourceModal/ConfirmationModal.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import { Modal, Button, Form } from 'react-bootstrap';
+
+const Confirmation = ({}) => {
+  const [showConfirmation, setShowConfirmation] = useState(null);
+
+  //initial state should be null/not displayed
+
+  //onClick, should show confirmation
+};
+
+return (
+  <>
+    <h1>Thanks for sharing!</h1>
+
+    <h3>Your submission is under review.</h3>
+
+    <h4>Follow us & Spread the News!</h4>
+
+    <div>
+      <div></div>
+    </div>
+  </>
+);
+
+export default ConfirmationModal;
