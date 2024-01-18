f18cb467a1796ddde614b89ffa8109f79dcd29c7
Edwin Francois <edwinfrancois0gmail.com>
2024-01-17 22:34:51 -0500

    Removed some unused imports and variables

--- src/components/AddResourceModal/ChooseResource.jsx
+++ src/components/AddResourceModal/ChooseResource.jsx
@@ -1,35 +1,17 @@
 import React from 'react';
 import styles from './AddResourceModal.module.scss';
 import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import {
-  CHANGE_PHLASK_TYPE,
-  PHLASK_TYPE_NONE,
-  PHLASK_TYPE_BATHROOM,
-  PHLASK_TYPE_FOOD,
-  PHLASK_TYPE_FORAGING,
-  PHLASK_TYPE_WATER,
-  TOOLBAR_MODAL_RESOURCE
-} from '../../actions/actions';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { ReactComponent as WaterIconCR } from '../icons/WaterIconChooseResource.svg';
 import { ReactComponent as FoodIconCR } from '../icons/FoodIconChooseResource.svg';
 import { ReactComponent as ForagingIconCR } from '../icons/ForagingIconChooseResource.svg';
 import { ReactComponent as ToiletIconCR } from '../icons/ToiletIconChooseResource.svg';
-import { useMediaQuery } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
 import { isMobile } from 'react-device-detect';
 
 function ChooseResource({ setFormStep }) {
   const dispatch = useDispatch();
-  const toolbarModal = useSelector(state => state.toolbarModal);
 
-  const changePhlaskType = phlaskType => {
-    dispatch({ type: 'SET_TOOLBAR_MODAL', phlaskType: phlaskType });
-  };
-
-  const theme = useTheme();
   return (
     <div className={isMobile ? styles.dialog : styles.dialogDesktop}>
       <h2 className={isMobile ? styles.greyHeader : styles.greyHeaderDesktop}>
