53f47a9616620e8ada941e74199839159c7d15fb
Ravi Makhija <ravi.makhija.pi@gmail.com>
2024-06-26 08:30:03 -0400

    Remove currently unnecessary isMobile condition
    Currently the isMobile condition occurs on the parent component,
    and so is not needed in DesktopChooseResource.js.

--- src/components/ChooseResource/DesktopChooseResource.js
+++ src/components/ChooseResource/DesktopChooseResource.js
@@ -1,6 +1,5 @@
 import { Box, Button, Collapse, Paper } from '@mui/material';
 import React, { useRef } from 'react';
-import { isMobile } from 'react-device-detect';
 import { connect, useSelector } from 'react-redux';
 import {
   TOOLBAR_MODAL_RESOURCE,
@@ -61,66 +60,63 @@ function DesktopChooseResource(props) {
 
   return (
     <>
-      {!isMobile && (
-        <Paper
-          sx={{
-            position: 'absolute',
-            left: '32px',
-            bottom: '133px',
-            width: '686px',
-            borderRadius: '10px'
-          }}
-          ref={ref}
+      <Paper
+        sx={{
+          position: 'absolute',
+          left: '32px',
+          bottom: '133px',
+          width: '686px',
+          borderRadius: '10px'
+        }}
+        ref={ref}
+      >
+        <Collapse
+          in={toolbarModal == TOOLBAR_MODAL_RESOURCE}
+          orientation="vertical"
+          timeout="auto"
         >
-          <Collapse
-            in={toolbarModal == TOOLBAR_MODAL_RESOURCE}
-            orientation="vertical"
-            timeout="auto"
-          >
-            <Box sx={{ padding: '40px' }}>
-              <Box>
-                <h1 className={styles.header}>Show Resource Type</h1>
-                <p className={styles.description}>
-                  Choose the type of resource you
-                  <br />
-                  would like to see on screen
-                </p>
-              </Box>
-              <Box sx={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
-                <ResourceButton
-                  icon={WaterIcon}
-                  color="#5286E9"
-                  text="Water"
-                  onClick={() => props.setResourceType(WATER_RESOURCE_TYPE)}
-                  data-cy="button-water-data-selector"
-                />
-                <ResourceButton
-                  icon={ForagingIcon}
-                  color="#5DA694"
-                  text="Foraging"
-                  onClick={() => props.setResourceType(FORAGE_RESOURCE_TYPE)}
-                  data-cy="button-foraging-data-selector"
-                />
-                <ResourceButton
-                  icon={FoodIcon}
-                  color="#FF9A55"
-                  text="Food"
-                  onClick={() => props.setResourceType(FOOD_RESOURCE_TYPE)}
-                  data-cy="button-food-data-selector"
-                />
-                <ResourceButton
-                  icon={BathroomIcon}
-                  color="#9E9E9E"
-                  text="Bathroom"
-                  onClick={() => props.setResourceType(BATHROOM_RESOURCE_TYPE)}
-                  data-cy="button-bathroom-data-selector"
-                />
-              </Box>
+          <Box sx={{ padding: '40px' }}>
+            <Box>
+              <h1 className={styles.header}>Show Resource Type</h1>
+              <p className={styles.description}>
+                Choose the type of resource you
+                <br />
+                would like to see on screen
+              </p>
             </Box>
-          </Collapse>
-        </Paper>
-      )}
-      {isMobile && <Paper></Paper>}
+            <Box sx={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
+              <ResourceButton
+                icon={WaterIcon}
+                color="#5286E9"
+                text="Water"
+                onClick={() => props.setResourceType(WATER_RESOURCE_TYPE)}
+                data-cy="button-water-data-selector"
+              />
+              <ResourceButton
+                icon={ForagingIcon}
+                color="#5DA694"
+                text="Foraging"
+                onClick={() => props.setResourceType(FORAGE_RESOURCE_TYPE)}
+                data-cy="button-foraging-data-selector"
+              />
+              <ResourceButton
+                icon={FoodIcon}
+                color="#FF9A55"
+                text="Food"
+                onClick={() => props.setResourceType(FOOD_RESOURCE_TYPE)}
+                data-cy="button-food-data-selector"
+              />
+              <ResourceButton
+                icon={BathroomIcon}
+                color="#9E9E9E"
+                text="Bathroom"
+                onClick={() => props.setResourceType(BATHROOM_RESOURCE_TYPE)}
+                data-cy="button-bathroom-data-selector"
+              />
+            </Box>
+          </Box>
+        </Collapse>
+      </Paper>
     </>
   );
 }
