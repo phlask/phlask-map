fbc7804d619b7e48b9acf66966ce0a04cd24595a
Ravi Makhija <ravi@palmtreecode.com>
2024-08-12 02:49:52 +0000

    Clean up work, restore prior test for desktop

--- cypress/e2e/desktop/siteInfo.cy.js
+++ cypress/e2e/desktop/siteInfo.cy.js
@@ -21,21 +21,17 @@ describe('site info', () => {
     // Switch to food view
     cy.get('[data-cy=button-resource-type-menu]').click()
     cy.get('[data-cy=button-FOOD-data-selector]').click()
+    cy.get('[data-cy=button-resource-type-menu]').click()
 
     // Load a sample food site
     // This is currently using live data, but should be updated to make use of test data.
     cy.get('[title=data-cy-1]').click();
+    cy.get('[title=data-cy-2]').click();
+
     cy.get('[data-cy=tap-organization-name]').should(
       'have.text',
       'Test Organization'
     );
-    // Confirm that the modal represents a half-modal
-    // Expand the modal to a full-modal
-    // Confirm that the correct description for the location is displayed
-    // Confirm that the modal represents a full-modal
-
-
-    // cy.get('[title=data-cy-2]').click();
   });
 
   it('should successfully display a foraging site', () => {
