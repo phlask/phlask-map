84c2adb39a1a52bdd02a3b1f0a17765bb48eba85
Cedric Fitzgerald <soulcedric2@gmail.com>
2024-08-13 19:22:10 -0400

    Fix map snapback by cleaning up hooks (#501)
    * Fix map snapback by cleaning up hooks
    
    * Re-add user-based geolocation
    
    * Remove console statement

--- src/components/ReactGoogleMaps/ReactGoogleMaps.js
+++ src/components/ReactGoogleMaps/ReactGoogleMaps.js
@@ -165,8 +165,8 @@ export const ReactGoogleMaps = ({ google }) => {
   );
   const toolbarModal = useSelector(state => state.filterMarkers.toolbarModal);
 
-  const [currentLat, setCurrentLat] = useState(mapCenter.lat);
-  const [currentLon, setCurrentLon] = useState(mapCenter.lng);
+  const [currentLat, setCurrentLat] = useState(CITY_HALL_COORDINATES.latitude);
+  const [currentLon, setCurrentLon] = useState(CITY_HALL_COORDINATES.longitude);
   const [zoom, setZoom] = useState(16);
   const [searchedTap, setSearchedTap] = useState(null);
   const [map, setMap] = useState(null);
@@ -184,39 +184,19 @@ export const ReactGoogleMaps = ({ google }) => {
   }, [allResources.length, dispatch]);
 
   useEffect(() => {
-    const setDefaultLocation = () => {
-      setCurrentLat(parseFloat(CITY_HALL_COORDINATES.latitude));
-      setCurrentLon(parseFloat(CITY_HALL_COORDINATES.longitude));
+    const fetchCoordinates = async () => {
+      try {
+        const position = await getCoordinates();
+        setCurrentLat(position.coords.latitude);
+        setCurrentLon(position.coords.longitude);
+      } catch (error) {
+        // Do nothing
+      }
     };
-    getCoordinates()
-      .then(position => {
-        if (
-          Number.isNaN(position.coords.latitude) ||
-          Number.isNaN(position.coords.longitude)
-        ) {
-          setDefaultLocation();
-        } else {
-          dispatch(
-            setMapCenter({
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            })
-          );
-          dispatch(
-            setUserLocation({
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            })
-          );
-          setCurrentLat(position.coords.latitude);
-          setCurrentLon(position.coords.longitude);
-        }
-      })
-      .catch(() => {
-        setDefaultLocation();
-      });
-  }, [dispatch]);
 
+    fetchCoordinates();
+  }, []);
+  
   //toggle window goes here
   const onMarkerClick = (resource, markerProps) => {
     dispatch(
@@ -226,7 +206,10 @@ export const ReactGoogleMaps = ({ google }) => {
       })
     );
     dispatch(setSelectedPlace(resource));
+    setCurrentLat(resource.latitude);
+    setCurrentLon(resource.longitude);
     markerProps.map.panTo({ lat: resource.latitude, lng: resource.longitude });
+
   };
 
   const onReady = (_, map) => {
@@ -305,10 +288,6 @@ export const ReactGoogleMaps = ({ google }) => {
             rotateControl={false}
             fullscreenControl={false}
             onReady={onReady}
-            initialCenter={{
-              lat: currentLat,
-              lng: currentLon
-            }}
             center={{
               lat: currentLat,
               lng: currentLon
