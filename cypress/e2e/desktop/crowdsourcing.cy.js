efe0c94e48e5ef0e5da61c678519f0a1d55869bf
Patrick Brady <patrick.brady@osmind.org>
2024-12-11 08:43:30 -0500

    centralize checkbox clicking logic

--- cypress/e2e/desktop/crowdsourcing.cy.js
+++ cypress/e2e/desktop/crowdsourcing.cy.js
@@ -5,6 +5,10 @@
 // All tap information
 // A submission with only one of each optional field for each resource type
 describe("crowdsourcing form", () => {
+  function clickInputByName(name) {
+    cy.get(`input[name="${name}"]`).click({ force: true })
+  }
+
   beforeEach(() => {
     cy.visit("/");
 
@@ -13,6 +17,9 @@ describe("crowdsourcing form", () => {
   });
 
   it("should successfully submit a water site for testing", () => {
+    const sourceTypes = ['drinkingFountain', 'sink', 'sodaMachine', 'waterCooler']
+    const helpfulInfoTypes = ['handicapAccessible', 'waterVesselNeeded']
+
     // Load the form
     cy.get('[data-cy=button-WATER-data-submit-selector]').click()
 
@@ -23,14 +30,10 @@ describe("crowdsourcing form", () => {
     cy.get('div[id="entry"]').click({ force: true })
     cy.get('li[data-value="Open access"]').click()
     cy.get('svg[data-testid="ExpandMoreIcon"]').click()
-    cy.get('input[name="drinkingFountain"]').click({ force: true })
-    cy.get('input[name="sink"]').click()
-    cy.get('input[name="sodaMachine"]').click()
-    cy.get('input[name="waterCooler"]').click()
+    sourceTypes.forEach(clickInputByName)
     cy.get('svg[data-testid="ExpandMoreIcon"]').click({ force: true })
     cy.get('svg[data-testid="ArrowForwardIosIcon"]').click()
-    cy.get('input[name="handicapAccessible"]').click()
-    cy.get('input[name="waterVesselNeeded"]').click()
+    helpfulInfoTypes.forEach(clickInputByName)
     cy.get('textarea[name="guidelines"]').type("Cypress Test")
     // TODO Uncomment this and validate the post-submission screen content once
     //      we have implemented a mechanism to ensure tests do not actually send data to our live DB.
@@ -38,6 +41,10 @@ describe("crowdsourcing form", () => {
   });
 
   it("should successfully submit a food site for testing", () => {
+    const foodTypes= ['perishable', 'nonPerishable', 'prepared', 'foodTypeOther']
+    const distributionTypes= ['eatOnSite', 'delivery', 'pickUp', 'distributionTypeOther']
+    const helpfulInfoTypes = ['handicapAccessible', 'idRequired', 'childrenOnly', 'communityFridges']
+
     // Load the form
     cy.get('[data-cy=button-FOOD-data-submit-selector]').click()
 
@@ -48,21 +55,12 @@ describe("crowdsourcing form", () => {
     cy.get('div[id="organization"]').click({ force: true })
     cy.get('li[data-value="Non-profit"]').click()
     cy.get('[data-testid="foodType"] svg[data-testid="ExpandMoreIcon"]').click()
-    cy.get('input[name="perishable"]').click({ force: true })
-    cy.get('input[name="nonPerishable"]').click()
-    cy.get('input[name="prepared"]').click()
-    cy.get('input[name="foodTypeOther"]').click()
+    foodTypes.forEach(clickInputByName)
     cy.get('[data-testid="foodType"] svg[data-testid="ExpandMoreIcon"]').click()
     cy.get('[data-testid="distribution"] svg[data-testid="ExpandMoreIcon"]').click({ force: true })
-    cy.get('input[name="eatOnSite"]').click()
-    cy.get('input[name="delivery"]').click()
-    cy.get('input[name="pickUp"]').click()
-    cy.get('input[name="distributionTypeOther"]').click()
+    distributionTypes.forEach(clickInputByName)
     cy.get('svg[data-testid="ArrowForwardIosIcon"]').click()
-    cy.get('input[name="handicapAccessible"]').click()
-    cy.get('input[name="idRequired"]').click()
-    cy.get('input[name="childrenOnly"]').click()
-    cy.get('input[name="communityFridges"]').click()
+    helpfulInfoTypes.forEach(clickInputByName)
     cy.get('textarea[name="guidelines"]').type("Cypress Test Long Enough To See Behind the Nav Overlay")
   });
 
