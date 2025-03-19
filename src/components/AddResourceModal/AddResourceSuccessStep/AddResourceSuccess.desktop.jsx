0c15265707ee7d40133f4012357fb0fb0d40100d
Ron Braha <ron.braha@icloud.com>
2025-03-18 22:47:54 -0400

    clean up styles

--- src/components/AddResourceModal/AddResourceSuccessStep/AddResourceSuccess.desktop.jsx
+++ src/components/AddResourceModal/AddResourceSuccessStep/AddResourceSuccess.desktop.jsx
@@ -15,43 +15,25 @@ const AddResourceSuccessDesktop = ({ onClose }) => (
   >
     <PhillySkyline width="258.5" height="225.37" />
     <Typography
-      sx={{
-        fontFamily: 'Inter',
-        fontStyle: 'normal',
+      sx={theme => ({
         fontWeight: 600,
-        fontSize: '24.19px',
+        fontSize: theme.typography.pxToRem(24),
         lineHeight: '29px',
         textAlign: 'center',
         letterSpacing: '0.02em',
-
-        color: '#60718C',
-
-        flex: 'none',
-        order: 1,
-        flexGrow: 0
-      }}
+        color: theme.palette.global.darkUI.darkGrey2
+      })}
     >
       Thank you for your submission!
     </Typography>
     <Typography
-      sx={{
-        /* Type Scale/body1 */
-        fontFamily: 'Inter',
-        fontStyle: 'normal',
+      sx={theme => ({
         fontWeight: 500,
-        fontSize: '14px',
+        fontSize: theme.typography.pxToRem(14),
         lineHeight: '24px',
-        /* identical to box height, or 171% */
         textAlign: 'center',
-
-        /* UI/UI-8 */
-        color: '#60718C',
-
-        /* Inside auto layout */
-        flex: 'none',
-        order: 2,
-        flexGrow: 0
-      }}
+        color: theme.palette.global.darkUI.darkGrey2
+      })}
     >
       You should see your site pop up on the map in a few days
     </Typography>
