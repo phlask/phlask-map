2817c101cd631c3bc972f67c49328f51966c2c7c
Gabriel Cardona <2278918+gcardonag@users.noreply.github.com>
2025-03-25 19:49:49 -0400

    Adds Airtable-based contact form for feedback

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -4,6 +4,7 @@ import InstagramIcon from 'icons/SocialInstagram';
 import TwitterIcon from 'icons/SocialTwitter';
 import ContactSubmitImage from 'icons/PhlaskContactSubmit';
 import { useForm } from 'react-hook-form';
+import { Box } from '@mui/material';
 import styles from './Pages.module.scss';
 
 const ContactInput = forwardRef(
@@ -127,110 +128,17 @@ const Contact = () => {
   });
 
   return (
-    <div className={styles.page}>
-      <h1 className={styles.pageHeader} data-cy="contact-header">
-        Contact
-      </h1>
-      {!submitted && (
-        <div>
-          <h2
-            className={[styles.pageSubheader, styles.pageContactSection].join(
-              ' '
-            )}
-          >
-            Share Feedback
-          </h2>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <ContactInput
-              label="Name"
-              {...register('name', { required: true })}
-            />
-            <ContactInput
-              label="Email"
-              {...register('email', { required: true })}
-            />
-            <ContactInput
-              className={styles.FeedbackInput}
-              label="Feedback"
-              as="textarea"
-              rows="4"
-              {...register('feedback', { required: true })}
-            />
-
-            <ContactCheckbox
-              label="I’m interested in helping PHLASK with future research"
-              {...register('isInterestedInResearch')}
-            />
-
-            <button
-              type="submit"
-              disabled={!form.isValid}
-              className={styles.submitButton}
-            >
-              <span className={styles.buttonText}>Submit Feedback</span>
-            </button>
-          </form>
-        </div>
-      )}
-      {submitted && (
-        <div>
-          {/* Thank You Message Container */}
-          <div className={styles.thankYouMessageContainer}>
-            <div className={styles.thankYouMessageSpacer} />
-            <ContactSubmitImage />
-            <h1 className={styles.pageHeader}>Thanks for your feedback!</h1>
-            <div className={styles.thankYouMessageSpacer} />
-          </div>
-
-          {/* Follow PHLASK, Connect, Social Media Icons, and Contact Email */}
-          <div className={styles.gridContainer}>
-            <h2 className={`${styles.pageSubheader} ${styles.followHeader}`}>
-              Follow PHLASK
-            </h2>
-            <h2 className={`${styles.pageSubheader} ${styles.connectHeader}`}>
-              Connect
-            </h2>
-            <div className={styles.socialLinksContainer}>
-              <a
-                aria-label="Like PHLask on Facebook"
-                target="_blank"
-                rel="noreferrer noopener"
-                href="https://www.facebook.com/PHLASKecosystem/"
-                className={styles.socialLink}
-              >
-                <FacebookIcon />
-              </a>
-              <a
-                aria-label="Follow PHLask on Instagram"
-                target="_blank"
-                rel="noreferrer noopener"
-                href="https://www.instagram.com/phlaskecosystem/"
-                className={styles.socialLink}
-              >
-                <InstagramIcon />
-              </a>
-              <a
-                aria-label="Follow PHLask on Twitter"
-                target="_blank"
-                rel="noreferrer noopener"
-                href="https://twitter.com/PHLASKecosystem/"
-                className={styles.socialLink}
-              >
-                <TwitterIcon />
-              </a>
-            </div>
-            <p className={`${styles.pageText} ${styles.otherInquiries}`}>
-              For all other inquiries, email{' '}
-              <a
-                href="mailto:phlaskecosystem@gmail.com"
-                className={styles.inquiryLink}
-              >
-                phlaskecosystem@gmail.com
-              </a>
-            </p>
-          </div>
-        </div>
-      )}
+    <div className={styles.page} style={{ 'overflow-y': 'hidden' }}>
+      <Box sx={{ height: '600px' }}>
+        <iframe
+          title="Contact Us"
+          className="airtable-embed"
+          src="https://airtable.com/embed/appyNdhZZn3gpovFh/pagDtKnlb6n3mCpgd/form"
+          width="100%"
+          height="600px"
+          style={{ background: 'transparent', border: 'none' }}
+        />
+      </Box>
     </div>
   );
 };
