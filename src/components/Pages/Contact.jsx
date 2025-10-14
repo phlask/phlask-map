7f445b9b2678e32ac37c67d69de8533d56da50b0
Raul Bazan <47358801+RaulBSanchez@users.noreply.github.com>
2025-10-14 19:47:59 -0400

    Added a progress indicator to Contact component (#647)

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,17 +1,43 @@
 import { forwardRef, useId, useState } from 'react';
-import { Box } from '@mui/material';
+import { Box, CircularProgress } from '@mui/material';
 
-const Contact = () => (
-  <Box sx={{ height: '600px' }}>
-    <iframe
-      title="Contact Us"
-      className="airtable-embed"
-      src="https://airtable.com/embed/appyNdhZZn3gpovFh/pagDtKnlb6n3mCpgd/form"
-      width="100%"
-      height="600px"
-      style={{ background: 'transparent', border: 'none' }}
-    />
-  </Box>
-);
+const Contact = () => {
+  const [loading, setLoading] = useState(true);
+
+  const handleLoading = () => {
+    setLoading(false);
+  };
+
+  return (
+    <Box sx={{ height: '600px' }}>
+      {loading && (
+        <Box
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      )}
+
+      <iframe
+        title="Contact Us"
+        className="airtable-embed"
+        src="https://airtable.com/embed/appyNdhZZn3gpovFh/pagDtKnlb6n3mCpgd/form"
+        width="100%"
+        height="600px"
+        style={{ background: 'transparent', border: 'none' }}
+        onLoad={handleLoading}
+      />
+    </Box>
+  );
+};
 
 export default Contact;
