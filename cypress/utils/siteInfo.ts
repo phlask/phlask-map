45ab5853b73f77700f4f4fa331db982220972d0e
Ron Braha <ron.braha@icloud.com>
2026-01-13 23:56:14 -0500

    try separating

--- cypress/utils/siteInfo.ts
+++ cypress/utils/siteInfo.ts
@@ -1,16 +1,14 @@
 export const waitForResourceModal = () => {
-  cy.wait('@resourceRequest', { timeout: 6000 }).then(() => {
-    cy.get('[data-cy="tap-organization-name"]', { timeout: 6000 }).should(
-      'exist'
-    );
-  });
+  cy.wait('@resourceRequest', { timeout: 6000 });
+  cy.get('[data-cy="tap-organization-name"]', { timeout: 6000 }).should(
+    'exist'
+  );
 };
 
 export const waitForMarker = () => {
-  cy.wait('@resourcesRequest', { timeout: 6000 })
-    .get('[data-cy="marker-1"]')
-    .parent()
-    .click({ force: true });
+  cy.wait('@resourcesRequest', { timeout: 6000 });
+  cy.get('[data-cy="marker-1"]').parent().scrollIntoView();
+  cy.get('[data-cy="marker-1"]').parent().click({ force: true });
   cy.location('search').should('contain', 'r=');
 };
 
