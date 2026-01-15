eef906c218f2fa8ddf5b4f916ca33936c13c8441
Ron Braha <ron.braha@icloud.com>
2026-01-15 00:41:03 -0500

    hopefully fixed modals

--- cypress/e2e/mobile/modals.cy.ts
+++ cypress/e2e/mobile/modals.cy.ts
@@ -1,10 +1,31 @@
 // For each resource type, load a site's half modal and full modal
 
-import {
-  clickOnFirstMarker,
-  waitForMarker,
-  waitForResourceModal
-} from '../../utils/siteInfo.ts';
+const waitForResourcesToLoad = () => {
+  cy.wait('@getResourcesRequest', { timeout: 6000 });
+};
+
+const selectResourceFromMenu = (
+  type: 'WATER' | 'FOOD' | 'FORAGE' | 'BATHROOM'
+) => {
+  cy.get(`[data-cy="button-${type}-data-selector-mobile"]`).click();
+};
+
+const selectMarker = () => {
+  cy.get('[data-cy="marker-1"]').click({
+    force: true,
+    waitForAnimations: true,
+    timeout: 4000
+  });
+  cy.location('search').should('contain', 'r=');
+  cy.wait('@resourceByIdRequest', { timeout: 6000 });
+};
+
+const verifyResourceIsShown = () => {
+  cy.location('search').should('contain', 'r=');
+  cy.get('[data-cy="tap-organization-name"]', { timeout: 6000 }).should(
+    'exist'
+  );
+};
 
 // for each site modal, confirm the required modal mode buttons work and that the relevant information is displayed
 describe('modals', () => {
@@ -14,43 +35,49 @@ describe('modals', () => {
 
     cy.intercept({
       method: 'GET',
-      url: '/rest/v1/resources?select=id*'
-    }).as('resourcesRequest');
+      url: ' https://wantycfbnzzocsbthqzs.supabase.co/rest/v1/resources?select=id%2Cname%2Clatitude%2Clongitude%2Cresource_type%2Centry_type&resource_type=eq.*'
+    }).as('getResourcesRequest');
 
     cy.intercept({
       method: 'GET',
-      url: '/rest/v1/resources?select=*'
-    }).as('resourceRequest');
+      hostname: 'wantycfbnzzocsbthqzs.supabase.co',
+      pathname: '/rest/v1/resources',
+      query: {
+        select: '*',
+        id: 'eq.*'
+      }
+    }).as('resourceByIdRequest');
 
     cy.visit('/');
-    cy.wait('@resourcesRequest', { timeout: 6000 });
+    waitForResourcesToLoad();
 
     cy.get('[data-cy="button-resource-type-menu"]').click();
   });
 
   it('should successfully display a water site', () => {
-    cy.get('[data-cy="button-WATER-data-selector-mobile"]').click();
-    clickOnFirstMarker();
-    waitForResourceModal();
+    selectResourceFromMenu('WATER');
+    selectMarker();
+    verifyResourceIsShown();
   });
 
   it('should successfully display a food site', () => {
-    cy.get('[data-cy="button-FOOD-data-selector-mobile"]').click();
-    waitForMarker();
-    waitForResourceModal();
+    selectResourceFromMenu('FOOD');
+    waitForResourcesToLoad();
+    selectMarker();
+    verifyResourceIsShown();
   });
 
   it('should successfully display a foraging site', () => {
-    cy.get('[data-cy="button-FORAGE-data-selector-mobile"]');
-
-    waitForMarker();
-    waitForResourceModal();
+    selectResourceFromMenu('FORAGE');
+    waitForResourcesToLoad();
+    selectMarker();
+    verifyResourceIsShown();
   });
 
   it('should successfully display a bathroom site', () => {
-    cy.get('[data-cy="button-BATHROOM-data-selector-mobile"]').click();
-
-    waitForMarker();
-    waitForResourceModal();
+    selectResourceFromMenu('BATHROOM');
+    waitForResourcesToLoad();
+    selectMarker();
+    verifyResourceIsShown();
   });
 });
