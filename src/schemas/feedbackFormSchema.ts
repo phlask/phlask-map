f60f8544f53b81e5c297e2443af44fdb098b1479
Anilkumar3494 <karapaanilkumar005@gmail.com>
2026-03-13 22:09:49 -0400

    fix schema and type

--- src/schemas/feedbackFormSchema.ts
+++ src/schemas/feedbackFormSchema.ts
@@ -1,9 +1,11 @@
 import * as z from 'zod';
 
 const feedbackFormSchema = z.object({
-  name: z.string().min(1, 'Name is required'),
+  name: z.string().min(2, 'Name is required with a minimum of 2 characters'),
   email: z.email().min(1, 'Email is required'),
-  feedback: z.string().min(1),
+  feedback: z
+    .string()
+    .min(10, 'Feedback is required with a minimum of 10 characters'),
   is_interested_in_contributing: z.boolean()
 });
 
