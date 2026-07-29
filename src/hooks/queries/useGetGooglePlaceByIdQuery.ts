16789372ea0145ce5f78cf5e7d4f784788c72182
Ron Braha <ron.braha@icloud.com>
2026-07-28 20:03:44 -0400

    nit

--- src/hooks/queries/useGetGooglePlaceByIdQuery.ts
+++ src/hooks/queries/useGetGooglePlaceByIdQuery.ts
@@ -1,19 +1,21 @@
 import { useQuery, skipToken } from '@tanstack/react-query';
 
-const useGetGooglePlaceByIdQuery = (placeId: string | null) => {
+const PLACE_FIELDS = [
+  'displayName',
+  'formattedAddress',
+  'addressComponents',
+  'types'
+];
+
+const useGetGooglePlaceByIdQuery = (id: string | null) => {
   const { data, isPending } = useQuery({
-    queryKey: ['place-id', placeId],
-    queryFn: placeId ? () =>
-      new google.maps.places.Place({
-        id: placeId
-      }).fetchFields({
-        fields: [
-          'displayName',
-          'formattedAddress',
-          'addressComponents',
-          'types'
-        ]
-      }).then(({ place }) => place) : skipToken,
+    queryKey: ['place-id', id],
+    queryFn: id
+      ? () =>
+          new google.maps.places.Place({ id })
+            .fetchFields({ fields: PLACE_FIELDS })
+            .then(({ place }) => place)
+      : skipToken
   });
 
   return { data, isPending };
